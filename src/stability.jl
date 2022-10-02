dd3aaee02747ea14b0fd022112230cd302c57886
reykboerner <reyk.boerner@fu-berlin.de>
2022-10-02 02:06:59 +0100

    minor changes to equilib

--- src/stability.jl
+++ src/stability.jl
@@ -8,7 +8,7 @@ function equilib(sys::StochSystem, state::Vector;
     dt=0.01,
     tmax=1e3,
     abstol=1e-5,
-    solver=nothing)
+    solver=Euler())
     
     condition(u, t, integrator) = norm(integrator.uprev-u) < abstol
     affect!(integrator) = terminate!(integrator)
@@ -17,7 +17,7 @@ function equilib(sys::StochSystem, state::Vector;
     prob = ODEProblem(sys.f, state, (0, tmax), p(sys))
     sol = solve(prob, solver; dt=dt, callback=equilib_cond, save_on=false, save_start=false)
 
-    sol.u
+    sol.u[1]
 end;
 
 function fixedpoints(sys::StochSystem, box)
