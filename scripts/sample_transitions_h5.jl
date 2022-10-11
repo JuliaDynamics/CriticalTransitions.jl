29519a72f566a5faf299cd0f3d6ba2f70b291357
reykboerner <reyk.boerner@fu-berlin.de>
2022-10-11 19:51:26 +0100

    improved output of sampling script

--- scripts/sample_transitions_h5.jl
+++ scripts/sample_transitions_h5.jl
@@ -12,17 +12,15 @@ and do data = h5open("filepath/filename.h5", "r").
 """
 
 # Load modules
-include("../src/CriticalTransitions.jl")
-using .CriticalTransitions, Printf, HDF5, Dates
-
+using CriticalTransitions, Printf, HDF5, Dates
 
 ###########################################################
 # SETTINGS ################################################
 ###########################################################
 
 # FitzHughNagumo parameters
-σ = 0.15               # noise intensity
-ϵ = 1.0                 # time scale parameter
+σ = 0.09               # noise intensity
+ϵ = 0.01                 # time scale parameter
 
 β, α, γ, κ, Ι = 3., 1., 1., 1., 0.
 pf = [ϵ, β, α, γ, κ, Ι]
@@ -32,24 +30,29 @@ pf = [ϵ, β, α, γ, κ, Ι]
 process = "WhiteGauss"  # noise process
 
 # Transition settings
-AtoB = true             # if false, transition B->A
+AtoB = false             # if false, transition B->A
 rad_i = 0.03            # ball radius around initial point
 rad_f = 0.1             # ball radius around final point
 
 # Run settings
-N = 10              	# number of transition samples
-Nmax = 1000              # maximum number of attempts
+N = 50              	# number of transition samples
+Nmax = 10000              # maximum number of attempts
 tmax = Int64(1e4)       # maximum simulation time per attempt
-dt = 0.02               # time step
+dt = 0.005               # time step
 solver = EM()		# SDEProblem solver
 
-# I/O settings
-filetext = "fhn_transAB_eps$(@sprintf "%.2e" ϵ)_noise$(@sprintf "%.2e" σ)_eye_$(N)"
-save_path = "../data/"
+###########################################################
+# End of user settings. ###################################
+###########################################################
 
-###########################################################
-# End of settings. ########################################
-###########################################################
+# I/O settings
+if AtoB
+    direction = "AB"
+else
+    direction = "BA"
+end
+filetext = "fhn_trans$(direction)_eps$(@sprintf "%.2e" ϵ)_noise$(@sprintf "%.2e" σ)_eye_$(N)"
+save_path = "../data/"
 
 # Instantiate system
 sys = StochSystem(FitzHughNagumo, pf, 2, σ, idfunc, nothing, Σ, process)
@@ -63,7 +66,7 @@ if AtoB
 end
 
 # I/O info
-info0 = "$(N) transition samples of $(sys.f) system with noise intensity $(sys.σ)."
+info0 = "$(N) transition samples of $(sys.f) system with noise intensity $(sys.σ) and time scale parameter $(ϵ)."
 info1 = "Start/end conditions:\n Initial state: $(A), ball radius $(rad_i)\n Final state: $(B), ball radius $(rad_f)"
 info2 = "Run details:\n time step dt=$(dt), maximum duration tmax=$(tmax), solver=$(string(solver))"
 info3 = "Dataset info:\n paths: transition path arrays of dimension (state × time), where a state is a vector [u,v]\n times: arrays of time values of the corresponding paths"
@@ -88,13 +91,14 @@ attributes(file)["data_info"] = info3
 
 # Call transitions function
 tstart = now()
-samples, times, idx = transitions(sys, A, B, N;
+samples, times, idx = transitions(sys, B, A, N;
     rad_i=rad_i, rad_f=rad_f, dt=dt, tmax=tmax, solver=solver,
     savefile=file, Nmax=Nmax)
 
 # Finalize file
 runtime = canonicalize(Dates.CompoundPeriod(Millisecond(now()-tstart)))
 attributes(file)["run_info"] = info0*"\n"*info1*"\n"*info2*"\nRuntime: $(runtime)"
+write(file, "success_fraction", length(idx)/Nmax)
 write(file, "path_numbers", sort(idx))
 
 # Close file
