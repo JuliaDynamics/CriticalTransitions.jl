89905b8853e8c9ff2b5f9576ddb3787e55396376
reykboerner <reyk.boerner@fu-berlin.de>
2022-10-13 22:26:56 +0100

    added docstring

--- src/systemanalysis/stability.jl
+++ src/systemanalysis/stability.jl
@@ -1,8 +1,23 @@
 include("../StochSystem.jl")
 
+"""
+    equilib(sys::StochSystem, state::State; kwargs...)
+Returns the equilibrium solution of the system `sys` for given initial condition `state`.
+
+Warning: This algorithm simply evolves the deterministic system forward in time until a steady-state condition is satisfied.
+Thus, the algorithm may output a false solution if it gets stuck in a quasi-equilibrium, or slowly evolving state.
+For more robust results, use `fixedpoints`.
+
+## Keyword arguments:
+* `abstol = 1e-5`: steady-state condition. Simulation ends when the rate of change (Euclidean distance in state space) of the state falls below `abstol`.
+* `tmax = 1e5`: maximum simulation time before the algorithm stops even if the steady-state condition is not reached.
+* `dt = 0.01`: time step of the ODE solver.
+* `solver = Euler()`: ODE solver used for evolving the state.
+"""
+
 function equilib(sys::StochSystem, state::State;
     dt=0.01,
-    tmax=1e3,
+    tmax=1e5,
     abstol=1e-5,
     solver=Euler())
     
