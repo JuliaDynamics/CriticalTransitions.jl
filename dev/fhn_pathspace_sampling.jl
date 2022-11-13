5366b1fc9cfe110fe78057d718a6f796212c2118
reykboerner <reyk.boerner@reading.ac.uk>
2022-11-13 00:24:21 +0000

    corrected factor 2 and noise intensity, added init argument

--- dev/fhn_pathspace_sampling.jl
+++ dev/fhn_pathspace_sampling.jl
@@ -41,10 +41,14 @@ function FitzHughNagumoSPDE(u, p, t)
     for i in 2:N-1
         
         # variable u
-        du[i] = 1/a^2 * central2(u,i,dz) + (1+κ/(ϵ*a^2)) * central(u,i+N,dz) - 2/(ϵ^2*a^2)*(-α*u[i]^3+γ*u[i]-κ*u[i+N]+Ι)*(-3*α*u[i]^2+γ) + 2*(β*u[i+N]-u[i]) + σ^2*(3*α*u[i]/ϵ)
+        du[i] = (1/a * central2(u,i,dz) + (1+κ/(ϵ*a)) * central(u,i+N,dz)
+                    - 1/(ϵ^2*a)*(-α*u[i]^3+γ*u[i]-κ*u[i+N]+Ι)*(-3*α*u[i]^2+γ)
+                    + 1*(β*u[i+N]-u[i]) + σ^2*(3*α*u[i]/ϵ))
 
         # variable v
-        du[i+N] = central2(u,i+N,dz) - (1+κ/(ϵ*a^2))*central(u,i,dz) + 2*κ/(ϵ^2*a^2)*(-α*u[i]^3+γ*u[i]-κ*u[i+N]+Ι) + 2*β*(-β*u[i+N]+u[i])
+        du[i+N] = (central2(u,i+N,dz) - (1+κ/(ϵ*a))*central(u,i,dz)
+                    + 1*κ/(ϵ^2*a)*(-α*u[i]^3+γ*u[i]-κ*u[i+N]+Ι)
+                    + 1*β*(-β*u[i+N]+u[i]))
     end
 
     SVector{length(u)}(du)
@@ -54,27 +58,29 @@ end
     fhn_pathspace_sampling(ϵ, σ, T, dz; kwargs...)
 Integrates the `FitzHughNagumoSPDE` in virtual time to generate pathspace Langevin MCMC samples.
 
-The initial condition is a straight line between the fixed points R and L (for the default FHN parameters).
+Unless specified otherwise, the initial condition is a straight line between the fixed points
+R and L (for the default FHN parameters).
 
 ## Arguments
-* ϵ (float): time scale parameter
-* σ (float): noise strength
-* T (float): total physical time
-* dz (float): physical time step
+* `ϵ` (float): time scale parameter
+* `σ` (float): noise strength
+* `T` (float): total physical time
+* `dz` (float): physical time step
+* `init=nothing`: initial path. If nothing, `init` is the straight line between the attractors.
 
 ## Keyword arguments
-* dt (float): virtual time step
-* tmax (float): total virtual time
-* a (float): first diagonal element of covariance matrix
-* start (string): Starting point of the path. Either "R" or "L" (default "R")
-* output_every (int): save the path at every `output_every`-th instance in virtual time
-* β, α, γ, κ, Ι: FHN parameters
+* `dt` (float): virtual time step
+* `tmax` (float): total virtual time
+* `a` (float): first diagonal element of covariance matrix
+* `start` (string): Starting point of the path. Either "R" or "L" (default "R")
+* `save_every` (int): save the path at every `save_every`-th instance in virtual time
+* `β, α, γ, κ, Ι`: FHN parameters
 
 ## Returns
 A list of two matrices, each with columns = coordinates and rows = virtual time instances.
 The first matrix contains the u coordinates, the second matrix the v coordinates.
 """
-function fhn_pathspace_sampling(ϵ, σ, T, dz;
+function fhn_pathspace_sampling(ϵ, σ, T, dz, init=nothing;
     dt=0.001,
     tmax=10.0,
     a=1.0,
@@ -83,8 +89,8 @@ function fhn_pathspace_sampling(ϵ, σ, T, dz;
     γ=1.0,
     κ=1.0,
     Ι=0.0,
-    start="R",
-    output_every=100)
+    save_every=100,
+    burnin=10)
 
     # number of points to discretize path
     N = Int(T/dz) + 1
@@ -96,29 +102,22 @@ function fhn_pathspace_sampling(ϵ, σ, T, dz;
     cov[N+1,N+1] = 0
     cov[2N, 2N] = 0
 
-    # Setup SPDE problem
-    spde(eps, sigma) = StochSystem(FitzHughNagumoSPDE, [eps, β, α, γ, κ, Ι, a, sigma, dz], 2N, sqrt(2)*sigma, idfunc, nothing, cov, "WhiteGauss")
-
-    # fixed points
-    R = [sqrt(2/3), sqrt(2/27)]
-    L = - [sqrt(2/3), sqrt(2/27)]
-    
-    if start=="R"
-        x_i, x_f = R, L
-    elseif start=="L"
-        x_i, x_f = L, R
-    end
-
     # initial path
-    init_path = zeros(2, N)
-    for i in 1:N
-        init_path[:,i] = x_i .+ (i-1)/(N-1).*(x_f .- x_i)
-    end
-    init = Vector(vec(init_path'))
+    if init == nothing
+        # fixed points
+        R = [sqrt(2/3), sqrt(2/27)]
+        L = - [sqrt(2/3), sqrt(2/27)]
 
-    # solve SPDE with Euler-Mayurama
+        rr, ll = -range(L[1], R[1], length=N), -range(L[2], R[2], length=N)
+        init = vcat(rr,ll)
+    end
+
+    # Setup SPDE problem
+    spde(eps, sigma) = StochSystem(FitzHughNagumoSPDE, [eps, β, α, γ, κ, Ι, a, sigma, dz],
+    2N, sqrt(2/dz)*sigma, idfunc, nothing, cov, "WhiteGauss")
+
     sim = simulate(spde(ϵ,σ), init, dt=dt, tmax=tmax)
     println("$(N) path points, $(round(Int, tmax/dt)) virtual time points, simulation status: $(sim.retcode)")
 
-    sim[1:N,1:output_every:end], sim[N+1:2N,1:output_every:end]
+    sim[1:N,1:save_every:end], sim[N+1:2N,1:save_every:end]
 end
