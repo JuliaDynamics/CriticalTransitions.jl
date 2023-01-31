a3ad83af130bd134dabff5a3f9ae6b8c65280fc3
ryandeeley <ryan.deeley@uni-oldenburg.de>
2023-01-31 18:08:30 +0100

    Exported get_res_times function.

--- src/CriticalTransitions.jl
+++ src/CriticalTransitions.jl
@@ -50,6 +50,6 @@ export EM, I # functions inherited from dependencies
 
 export FitzHughNagumoSPDE, fhn_pathspace_sampling
 export langevinmcmc_spde, symbolise_spde, langevinmcmc
-export residence_time, residence_times, ResTimes, temporal, runandsavetimes
+export residence_time, residence_times, ResTimes, temporal, runandsavetimes, get_res_times
 
 end # module CriticalTransitions
