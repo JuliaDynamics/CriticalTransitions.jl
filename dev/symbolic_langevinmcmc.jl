f93f516dc0e69e202ccf0f35bd1ce39b8566dd9e
ryandeeley <ryan.deeley@uni-oldenburg.de>
2023-02-01 17:26:13 +0100

    Created new Jacbian of StochSys function.

--- dev/symbolic_langevinmcmc.jl
+++ dev/symbolic_langevinmcmc.jl
@@ -82,7 +82,7 @@ function jacobian(sys::StochSystem)
 
 end
 
-function jacobian(sys:StochSystem, x::State)
+function jacobian(sys::StochSystem, x::State)
 
     # loading the Jacobian in symbolic form
     jacobian = jacobian(sys);
