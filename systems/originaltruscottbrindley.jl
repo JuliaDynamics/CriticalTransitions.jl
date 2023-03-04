0479e1953381d55e92ccd12e8683d1e41df31c92
ryandeeley <ryan.deeley@uni-oldenburg.de>
2023-03-04 14:00:23 +0100

    Added ramped original Truscott-Brindley shortcut function.

--- systems/originaltruscottbrindley.jl
+++ systems/originaltruscottbrindley.jl
@@ -74,10 +74,10 @@ end
     origtb_rσ(r, σ)
 A shortcut command for returning a StochSystem of the original Truscott-Brindley system in a default setup with multiplicative anisotropic noise. 
     
-This setup fixes the parameters K = 108, Rₘ = 0.7, α = 5.7, γ = 0.05, μ = 0.012 and leaves the values of the growth rate parameter r as a function argument. The prescribed noise process is multiplicative and anisotropic: the first variable is peturbed by Gaussian white noise realisations that are multiplied by the variable's current value; the second variable has no stochastic component. The noise strength σ is left as the remaining function argument.
+This setup fixes the parameters K = 108, Rₘ = 0.7, α = 5.7, γ = 0.05, μ = 0.012 and leaves the value of the growth rate parameter r as a function argument. The prescribed noise process is multiplicative and anisotropic: the first variable is peturbed by Gaussian white noise realisations that are multiplied by the variable's current value; the second variable has no stochastic component. The noise strength σ is left as the remaining function argument.
 """
 function origtb_rσ(r, σ) # a convenient three-parameter version of the modifiedtruscottbrindley system 
-    f(u,p,t) = modifiedtruscottbrindley(u,p,t);
+    f(u,p,t) = originaltruscottbrindley(u,p,t);
     K = 108; Rₘ = 0.7; α = 5.7; γ = 0.05; μ = 0.012; # standard parameters without α (growth rate) and ξ (time-scale separation)
     pf_wo_r = [K, Rₘ, α, γ, μ]; # parameters vector without α or ξ
     dim = 2;
@@ -87,3 +87,22 @@ function origtb_rσ(r, σ) # a convenient three-parameter version of the modifie
     process = "WhiteGauss";
     StochSystem(f, vcat([r], pf_wo_r), dim, σ, g, pg, Σ, process)
 end;
+
+"""
+    rorigtb_vTtrTraσ(v, Ttrans, Tramp, σ)
+A shortcut command for returning a StochSystem of the ramped original Truscott-Brindley system in a default setup with multiplicative anisotropic noise. 
+    
+This setup fixes the parameters K = 108, Rₘ = 0.7, α = 5.7, γ = 0.05, μ = 0.012 and leaves the evolution of the growth rate parameter r as a function argument. The prescribed noise process is multiplicative and anisotropic: the variables are peturbed by Gaussian white noise realisations that are multiplied by their current values - the diffusion matrix is [1, 0; 0, √γ]. The noise strength σ is left as the remaining function argument.
+"""
+function origtb_rσ(v, Ttrans, Tramp, σ) # a convenient three-parameter version of the modifiedtruscottbrindley system 
+    f(u,p,t) = rampedoriginaldtruscottbrindley(u,p,t);
+    K = 108; Rₘ = 0.7; α = 5.7; γ = 0.05; μ = 0.012; # standard parameters without α (growth rate) and ξ (time-scale separation)
+    pf_wo_r = [K, Rₘ, α, γ, μ]; # parameters vector without α or ξ
+    dim = 2;
+    g(u,p,t) = multiplicative_idx(u,p,t,[true,false]);
+    pg = nothing; 
+    Σ = [1 0; 0 √γ];
+    process = "WhiteGauss";
+    StochSystem(f, vcat(pf_wo_r, [v, Ttrans, Tramp]), dim, σ, g, pg, Σ, process)
+end;
+
