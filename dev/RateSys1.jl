f031ce6399b3da645d88f42ee3ba89d5e55512fe
ryandeeley <ryan.deeley@uni-oldenburg.de>
2023-03-18 02:56:06 +0100

    Cleaned up some stuff in the RateSystem struct.

--- dev/RateSys1.jl
+++ dev/RateSys1.jl
@@ -11,12 +11,14 @@ Defines a stochastic dynamical system with a rate dependent shift in `CriticalTr
 
 
 struct RateSystem
-    f::Function
-    pf::Parameters
-    L::Function     # Vector of functions describing the change of each parameter
-    T_trans::Float64
-    T_shift::Float64
-    dim::Int64
+    f::Function                 # vector of functions describing the derivatives of each state variable
+    pf::Parameters              # parameters for the derivatives of the state variables 
+    td_inds::Vector{Bool}       # a boolean vector indicating which parameters are time-dependent
+    L::Function                 # vector of functions describing the derivatives of each time-dependent parameter
+    pL::Parameters              # parameters for the derivatives of the parameter variables
+    T_trans::Float64            # the length of time before ramping begins
+    T_shift::Float64            # the length of time where the specified parameters evolve
+    dim::Int64                  
     σ::Float64
     g::Function
     pg::Parameters
@@ -58,13 +60,21 @@ function simulate(sys::RateSystem, init::State;
 end;
 
 function fL(u,p,t,sys::RateSystem)
-#    stationary = 0..t_trans ∪ t_trans+t_shift..Inf
-#    du = t in stationary ? hcat(sys.f(u,p,t),zeros(length(sys.pf)) : hcat(sys.f(u,p,t),sys.L(u,p,t))
 
-    nonstationary = t_trans..t_trans+t_shift
-    du = t in nonstationary ? hcat(sys.f(u,p,t),sys.L(u,p,t)) : hcat(sys.f(u,p,t),zeros(length(sys.pf))
-    return SVector{du}
+    #stationary = 0..t_trans ∪ t_trans+t_shift..Inf
+    #du = t in stationary ? hcat(sys.f(u,p,t),zeros(length(sys.pf)) : hcat(sys.f(u,p,t),sys.L(u,p,t))
+
+    # u is the vector made up of the state variables and time-dependent parameters
+    state_vars = u[1:sys.dim]; # the state-variables
+    param_vars = u[sys.dim+1:end]; # the (time-dependent) parameters
+
+    params = sys.pf; params[sys.td_inds] = param_vars; # combining the fixed parameters with the current values of the time-dependent parameters
+
+    nonstationary = t_trans..t_trans+t_shift; # the time-interval of the ramping period
+    
+    du = t in nonstationary ? hcat(sys.f(state_vars,params,t),sys.L(param_vars,sys.pL,t)) : hcat(sys.f(state_vars,param_vars,t),SVector{sum(sys.td_inds)}(zeros(sum(sys.td_inds))))
+
 end;
 
 
-stochtorate(sys::StochSystem,L::Function,T_trans::Float64,T_shift::Float64)=RateSystem(sys.f,sys.pf,L,T_trans,T_shift,sys.dim,sys.σ,sys.g,sys.pg,sys.Σ,sys.process)
+stochtorate(sys::StochSystem,td_inds::Vector{Bool},L::Function,pL::Vector,T_trans::Float64,T_shift::Float64)=RateSystem(sys.f,sys.pf,td_inds,L,pL,T_trans,T_shift,sys.dim,sys.σ,sys.g,sys.pg,sys.Σ,sys.process)
