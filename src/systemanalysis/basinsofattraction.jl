4a7c8b02076d322d18056516aea8ebcd275269eb
Ryan Deeley <ryan.deeley@uni-oldenburg.de>
2023-03-27 00:16:26 +0200

    Updated doc strings for the basins function.

--- src/systemanalysis/basinsofattraction.jl
+++ src/systemanalysis/basinsofattraction.jl
@@ -47,6 +47,11 @@ The plane is given by ``P_{U,V} := \\{A+u(B-A)+v(C-A): u \\in U,\\, v\\in V\\}``
 i.e. ``P_{U,\\,V}`` is the "largest" possible plane contained within H. This plane is determined behind the scenes.    
 
 This function returns a four-dimensional vector. The first two entries are discretised versions of the interval ``U`` and ``V`` (as defined above, of lengths ``\\ell_U,\\,\\ell_V`` respectively); the third entry is a dictionary of the attractors (stable equilibria) of the system within `H`, and the final entry is an ``\\ell_U\\times\\ell_V`` matrix of integers that group the initial conditions (written in terms of ``A+u(B-A)+v(C-A)`` where ``u\\in U`` and ``v\\in V``) by which attractor they will in time converge to. 
+
+## Keyword arguments 
+* `bstep = [0.01, 0.01]`: a vector of length two whose elements respectively specify the length of the incremental steps taken across each dimension in the discretisation of your plane
+* `pstep = [0.1, 0.1]`: a vector of length two whose elements give the increments of the mesh that the maximisation process of finding a plane from a box is taken over (for more information see the source code of the function `plane` in the src/systemanalysis/planeofbox.jl file)
+* `AVPparams=[0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)]`: a vector of length six specifying parameters for the DynamicalSystems.AttractorsViaProximity (namely, `ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, maxitstop, diffeq`)
 """ 
 function basins(sys::StochSystem, A, B, C, H; bstep::Vector = [0.01, 0.01], pstep::Vector = [0.1, 0.1], AVPparams = [0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)])
 
