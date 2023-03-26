52c9b4accbb99ef66bc4475c2c1cb1dcb4a91c7d
Ryan Deeley <ryand@tpZ13.fritz.box>
2023-03-26 23:21:57 +0200

    Improved doc strings for the basins function.

--- src/systemanalysis/basinsofattraction.jl
+++ src/systemanalysis/basinsofattraction.jl
@@ -37,21 +37,6 @@ end
 
 """
     basins(sys::StochSystem, A, B, C, H; kwargs...)
-# Computes the basins of attraction of StochSystem `sys` on a plane spanned by the points `A`, `B`, `C` and limited by the box `H`. Uses the AttractorsViaProximity function from DynamicalSystems.jl to compute the basins of attraction.
-
-# To be further documented.
-
-# `A`, `B`, `C` are elements of ``\mathbb{R}^d`` (where ``d`` is the dimension of the  `sys`) 
-# `H` is a hyperrectangle in ``\mathbb{R}^d``
-
-# The plane is given by ``P_{U,V}\coloneqq\{A+u(B-A)+v(C-A): u \in U,\, v\in V\}`` for some closed and bounded real intervals ``U`` and ``V`` which are selected such that both i) ``P_{U,\,V} \subseteq H`` and ii) ``U\times V\subseteq\mathbb{R}^2`` has maximal area, i.e. ``P_{U,\,V}`` is the "largest" possible plane contained within H. This plane is determined behind the scenes.    
-
-# This function returns a four-dimensional vector. The first two entries are discretised versions of the interval ``U`` and ``V`` (as defined above, of lengths ``\ell_U,\,\ell_V`` respectively); the third entry is a dictionary of the attractors (stable equilibria) of the system within `H`, and the final entry is an ``\ell_U\times\ell_V`` matrix of integers that group the initial conditions (written in terms of ``A+u(B-A)+v(C-A)`` where ``u\in U`` and ``v\in V``) by which attractor they will in time converge to. 
-
-# ## Keyword arguments 
-# * `bstep = [0.01, 0.01]`: a vector of length two whose elements respectively specify the length of the incremental steps taken across each dimension in the discretisation of your plane
-# * `pstep = [0.1, 0.1]`: a vector of length two whose elements give the increments of the mesh that the maximisation process of finding a plane from a box is taken over (for more information see the source code of the function `plane` in the src/systemanalysis/planeofbox.jl file)
-# * `AVPparams=[0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)]`: a vector of length six specifying parameters for the DynamicalSystems.AttractorsViaProximity (namely, `ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, maxitstop, diffeq`)
 """ 
 function basins(sys::StochSystem, A, B, C, H; bstep::Vector = [0.01, 0.01], pstep::Vector = [0.1, 0.1], AVPparams = [0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)])
 
