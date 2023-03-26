1fe763cf52692040d5284272bd40af9ecd2a6399
Ryan Deeley <ryand@tpZ13.fritz.box>
2023-03-26 23:17:39 +0200

    Improved doc strings for the basins function.

--- src/systemanalysis/basinsofattraction.jl
+++ src/systemanalysis/basinsofattraction.jl
@@ -51,7 +51,7 @@ This function returns a four-dimensional vector. The first two entries are discr
 ## Keyword arguments 
 * `bstep = [0.01, 0.01]`: a vector of length two whose elements respectively specify the length of the incremental steps taken across each dimension in the discretisation of your plane
 * `pstep = [0.1, 0.1]`: a vector of length two whose elements give the increments of the mesh that the maximisation process of finding a plane from a box is taken over (for more information see the source code of the function `plane` in the src/systemanalysis/planeofbox.jl file)
-* `AVPparams=[0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)]: a vector of length six specifying parameters for the DynamicalSystems.AttractorsViaProximity (namely, `ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, maxitstop, diffeq`)
+* `AVPparams=[0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)]`: a vector of length six specifying parameters for the DynamicalSystems.AttractorsViaProximity (namely, `ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, maxitstop, diffeq`)
 """ 
 function basins(sys::StochSystem, A, B, C, H; bstep::Vector = [0.01, 0.01], pstep::Vector = [0.1, 0.1], AVPparams = [0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)])
 
