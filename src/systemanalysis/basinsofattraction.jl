63e2b68ecabefd21a7f34c8bee0c3c32cf3e2ba6
Ryan Deeley <ryan.deeley@uni-oldenburg.de>
2023-03-27 04:11:49 +0200

    Added links to the documentation of the basins function.

--- src/systemanalysis/basinsofattraction.jl
+++ src/systemanalysis/basinsofattraction.jl
@@ -36,7 +36,7 @@ toattractors(V::Dataset) = Dict(i => Dataset([V[i]]) for i in 1:length(V))
 
 """
     basins(sys::StochSystem, A, B, C, H; kwargs...)
-Computes the basins of attraction of StochSystem `sys` on a plane spanned by the distinct points `A`, `B`, `C` and limited by the box `H`. Uses the AttractorsViaProximity function from DynamicalSystems.jl to compute the basins of attraction.
+Computes the basins of attraction of StochSystem `sys` on a plane spanned by the distinct points `A`, `B`, `C` and limited by the box `H`. Uses the [`AttractorsViaProximity`](https://juliadynamics.github.io/Attractors.jl/v1.2/attractors/#Attractors.AttractorsViaProximity) function from [`DynamicalSystems.jl`](https://juliadynamics.github.io/DynamicalSystems.jl/stable/) to compute the basins of attraction.
 
 `A`, `B`, `C` are elements of ``\\mathbb{R}^d`` (where ``d`` is the dimension of the  `sys`) and `H` is a hyperrectangle in ``\\mathbb{R}^d``.
 
@@ -49,8 +49,8 @@ This function returns a four-dimensional vector. The first two entries are discr
 
 ## Keyword arguments 
 * `bstep = [0.01, 0.01]`: a vector of length two whose elements respectively specify the length of the incremental steps taken across each dimension in the discretisation of your plane
-* `pstep = [0.1, 0.1]`: a vector of length two whose elements give the increments of the mesh that the maximisation process of finding a plane from a box is taken over (for more information see the source code of the function `plane` in the src/systemanalysis/planeofbox.jl file)
-* `AVPparams = [0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)]`: a vector of length six specifying parameters for the DynamicalSystems.AttractorsViaProximity function (namely, `ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, maxitstop, diffeq`)
+* `pstep = [0.1, 0.1]`: a vector of length two whose elements give the increments of the mesh that the maximisation process of finding a plane from a box is taken over (for more information see the source code of the function `plane` in the `src/systemanalysis/planeofbox.jl` file)
+* `AVPparams = [0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)]`: a vector of length six specifying parameters for the [`AttractorsViaProximity`](https://juliadynamics.github.io/Attractors.jl/v1.2/attractors/#Attractors.AttractorsViaProximity) function (namely, `ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, maxitstop, diffeq`)
 """ 
 function basins(sys::StochSystem, A, B, C, H; bstep::Vector = [0.01, 0.01], pstep::Vector = [0.1, 0.1], AVPparams = [0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)])
 
