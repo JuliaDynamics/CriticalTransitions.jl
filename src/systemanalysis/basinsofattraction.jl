100e5891249d753d39c4753f0b84debc55bb7923
Ryan Deeley <ryan.deeley@uni-oldenburg.de>
2023-03-27 03:57:54 +0200

    Cleaned up the basins function.

--- src/systemanalysis/basinsofattraction.jl
+++ src/systemanalysis/basinsofattraction.jl
@@ -1,80 +1,75 @@
 include("planeofbox.jl")
 
-function toattractors(V::Dataset)
-    Dict(i => Dataset([V[i]]) for i in 1:length(V))
-end
+toattractors(V::Dataset) = Dict(i => Dataset([V[i]]) for i in 1:length(V))
 
-function AVPthreads(X, Y, cds, attractors, p)
-    #AVP =  AVPtype(method, p)(cds, attractors);
-    ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, diffeq = p; 
-    AVP = AttractorsViaProximity(cds,attractors,ϵ;Ttr,Δt,horizon_limit,mx_chk_lost,diffeq)
-    A = zeros(length(Y), length(X));
-    @Threads.threads for ii ∈ 1:length(X)
-        Z = [[X[ii], y] for y ∈ Y];
-        A[:,ii] = AVP.(Z)
-    end
-    A
-end
+# function AVPthreads(A, B, C, X, Y, cds, attractors, p)
+#     # AVP =  AVPtype("defaultCT", p)(cds, attractors);
+#     ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, diffeq = p; 
+#     #AVP = AttractorsViaProximity(cds,attractors,ϵ;Ttr,Δt,horizon_limit,mx_chk_lost,diffeq)
+#     A = zeros(length(Y), length(X));
+#     @Threads.threads for ii ∈ tqdm(1:length(X))
+#         #Z = [[X[ii], y] for y ∈ Y];
+#         Z = [A+X[ii]*(B-A)+y*(C-A) for y ∈ Y]; # a row of initial conditions
+#         A[:,ii] = AttractorsViaProximity(cds,attractors,ϵ;Ttr,Δt,horizon_limit,mx_chk_lost,diffeq).(Z)
+#     end
+#     A
+# end
 
-function AVPtype(method::String, p::Vector)
-    if method == "defaultCT"
-        # (using Ryan's default values for AttractorsViaProximity)
-        opt1(x, y) = AttractorsViaProximity(x, y, 0.00005; Ttr = 1000, Δt = 0.001, mx_chk_lost = 100000, diffeq = (alg = Vern9(), abstol = 1e-16, reltol = 1e-16))
-    elseif method == "custom"
-        # # custom callAVP (using custom values for AttractorsViaProximity)
-        # the following seven lines of code would show you how to only specify some arguments, and leave others as preprescribed "standard values", but for now i don't this it's neccesary to implement
-        # q = []; # default values of ϵ, trans, dt, horlim, maxitstop, solver
-        # for ii ∈ 1:length(p)
-        #     if p[ii] != ~
-        #         q[ii] = p[ii]
-        #     end
-        # end
-        # ϵ, trans, dt, horlim, maxitstop, solver = q
-        ϵ, trans, dt, horlim, maxitstop, solver = p
-        opt2(x, y) = AttractorsViaProximity(x, y, ϵ; Ttr = trans, Δt = dt, horizon_limit = horlim, mx_chk_lost = maxitstop, diffeq = solver)
-    end
-end
+# function AVPtype(method::String, p::Vector)
+#     if method == "defaultCT"
+#         # (using Ryan's default values for AttractorsViaProximity)
+#         opt1(x, y) = AttractorsViaProximity(x, y, 0.00005; Ttr = 1000, Δt = 0.001, mx_chk_lost = 100000, diffeq = (alg = Vern9(), abstol = 1e-16, reltol = 1e-16))
+#     elseif method == "custom"
+#         # # custom callAVP (using custom values for AttractorsViaProximity)
+#         # the following seven lines of code would show you how to only specify some arguments, and leave others as preprescribed "standard values", but for now i don't this it's neccesary to implement
+#         # q = []; # default values of ϵ, trans, dt, horlim, maxitstop, solver
+#         # for ii ∈ 1:length(p)
+#         #     if p[ii] != ~
+#         #         q[ii] = p[ii]
+#         #     end
+#         # end
+#         # ϵ, trans, dt, horlim, maxitstop, solver = q
+#         ϵ, trans, dt, horlim, maxitstop, solver = p
+#         opt2(x, y) = AttractorsViaProximity(x, y, ϵ; Ttr = trans, Δt = dt, horizon_limit = horlim, mx_chk_lost = maxitstop, diffeq = solver)
+#     end
+# end
 
 """
     basins(sys::StochSystem, A, B, C, H; kwargs...)
-Computes the basins of attraction of StochSystem `sys` on a plane spanned by the points `A`, `B`, `C` and limited by the box `H`. Uses the AttractorsViaProximity function from DynamicalSystems.jl to compute the basins of attraction.
+Computes the basins of attraction of StochSystem `sys` on a plane spanned by the distinct points `A`, `B`, `C` and limited by the box `H`. Uses the AttractorsViaProximity function from DynamicalSystems.jl to compute the basins of attraction.
 
 `A`, `B`, `C` are elements of ``\\mathbb{R}^d`` (where ``d`` is the dimension of the  `sys`) and `H` is a hyperrectangle in ``\\mathbb{R}^d``.
 
-The plane is given by ``P_{U,V} := \\{A+u(B-A)+v(C-A): u \\in U,\\, v\\in V\\}`` for some closed and bounded real intervals ``U`` and ``V`` which are selected such that both 
+The plane is given by ``P_{U,V} := \\{A+u(B-A)+v(C-A)\\in\\mathbb{R}^d: u \\in U,\\, v\\in V\\}`` for some closed and bounded real intervals ``U`` and ``V`` which are selected such that both 
 * ``P_{U,\\,V} \\subseteq H``, and 
 * ``U\\times V\\subseteq\\mathbb{R}^2`` has maximal area, 
 i.e. ``P_{U,\\,V}`` is the "largest" possible plane contained within H. This plane is determined behind the scenes.    
 
-This function returns a four-dimensional vector. The first two entries are discretised versions of the interval ``U`` and ``V`` (as defined above, of lengths ``\\ell_U,\\,\\ell_V`` respectively); the third entry is a dictionary of the attractors (stable equilibria) of the system within `H`, and the final entry is an ``\\ell_U\\times\\ell_V`` matrix of integers that group the initial conditions (written in terms of ``A+u(B-A)+v(C-A)`` where ``u\\in U`` and ``v\\in V``) by which attractor they will in time converge to. 
+This function returns a four-dimensional vector. The first two entries are discretised versions of the interval ``U`` and ``V`` (as defined above, of lengths ``\\ell_U,\\,\\ell_V`` respectively); the third entry is a dictionary of the attractors (stable equilibria) of the system within `H`, and the final entry is an ``\\ell_V\\times\\ell_U`` matrix of integers that group the initial conditions (written in terms of ``A+u(B-A)+v(C-A)`` where ``u\\in U`` and ``v\\in V``) by which attractor they will in time converge to. 
 
 ## Keyword arguments 
 * `bstep = [0.01, 0.01]`: a vector of length two whose elements respectively specify the length of the incremental steps taken across each dimension in the discretisation of your plane
 * `pstep = [0.1, 0.1]`: a vector of length two whose elements give the increments of the mesh that the maximisation process of finding a plane from a box is taken over (for more information see the source code of the function `plane` in the src/systemanalysis/planeofbox.jl file)
-* `AVPparams=[0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)]`: a vector of length six specifying parameters for the DynamicalSystems.AttractorsViaProximity (namely, `ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, maxitstop, diffeq`)
+* `AVPparams = [0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)]`: a vector of length six specifying parameters for the DynamicalSystems.AttractorsViaProximity function (namely, `ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, maxitstop, diffeq`)
 """ 
 function basins(sys::StochSystem, A, B, C, H; bstep::Vector = [0.01, 0.01], pstep::Vector = [0.1, 0.1], AVPparams = [0.00005, 1000, 0.001, 1e3, 100000, (alg = Vern9(), abstol = 1e-16, reltol = 1e-16)])
 
-    # here H is a hyperrectangle contained in R^d
-    # bstep is the incremental steps you will take across your basin of attraction defined on some plane
-    # pstep is a vector of steps (increments) for mechanism behind finding a plane from a box
+    U, V = plane(A, B, C, H; step = pstep); # the intervals for the maximal plane P_{U,V}
 
-    projs = plane(A, B, C, H; step = pstep);
+    X = range(U[1], U[2]; step = bstep[1]); # the range of projections along the B-A direction 
+    Y = range(V[1], V[2]; step = bstep[2]); # the range of projections along the C-A direction
+    h = zeros(length(Y),length(X)); # an array to store the attractors for each initial condition
 
-    u = projs[1]; v = projs[2];
-
-    # note that the plane returns tuples u, v that tell us the min/max amount to project along the B-A and C-A direction
-
-    # the two dimensional range we loop over 
-    X = range(u[1], u[2]; step = bstep[1]); 
-    Y = range(v[1], v[2]; step = bstep[2]);
-
-    # the dictionary of attractors
-
+    # defining the parameters required to run the AttractorsViaProximity function
     fps = CriticalTransitions.fixedpoints(sys, H);
     attractors = toattractors(fps[1][fps[3]]);
+    ϵ, Ttr, Δt, horizon_limit, mx_chk_lost, diffeq = AVPparams; 
 
-    h = AVPthreads(X, Y, tocds(sys), attractors, AVPparams);
+    # running the AttractorsViaProximity function using parallel computing
+    @Threads.threads for ii ∈ tqdm(1:length(X))
+        Z = [A+X[ii]*(B-A)+y*(C-A) for y ∈ Y]; # a row of initial conditions
+        h[:,ii] = AttractorsViaProximity(tocds(sys),attractors,ϵ;Ttr,Δt,horizon_limit,mx_chk_lost,diffeq).(Z)
+    end
 
     [X, Y, attractors, h]
 
--- src/systemanalysis/planeofbox.jl
+++ src/systemanalysis/planeofbox.jl
@@ -49,70 +49,30 @@ function plane(A, B, C, box; step::Vector = [0.1, 0.1])
         @warn("Please enter vectors A, B, C and a set 'box' that are of the same dimension")
     elseif A == zeros(length(A)) == B || A == zeros(length(A)) == C
         @warn("Please enter vectors such that neither A=B=0 or A=C=0 hold") 
-    elseif normalise(B-A) == normalise(C-A)
+    elseif normalise(B-A) == normalise(C-A) # i.e. in particular B =/= C
         @warn("Please enter vectors B and C that are not collinear")
+    elseif A == B || A == C
+        @warn("Please enter distinct vectors A, B, and C.")
     else ## main code
+
         d = length(A); # the (shared) dimension that we are working within 
-        ## firstly we deal in the (B-A) direction 
         ϕₜ = []; # here i should define an empty vector in a better way! i'm later going to fill it with Interval{Float64}s
-        if A ≠ B
-            @Threads.threads for ii ∈ 1:d
-                if A[ii] ≠ B[ii]
-                    vals = ((box[ii].lo-A[ii])/(B[ii]-A[ii]), (box[ii].hi-A[ii])/(B[ii]-A[ii]));
-                    low = minimum(vals);
-                    high = maximum(vals);
-                    ϕₜ = push!(ϕₜ, low..high); # store the range of permitted t values for A + t (B-A) in the iith component 
-                end
-            end
-        else # i.e. A = B, this is a separate interpretation!!! for when you are considering A+t*B but B=A
-            @Threads.threads for ii ∈ 1:d
-                if A[ii] ≠ 0
-                    vals = (box[ii].lo/A[ii], box[ii].hi/A[ii]) 
-                    low = minimum(vals)
-                    high = maximum(vals)
-                    ϕₜ = push!(ϕₜ, low..high);
-                end
+            
+        @Threads.threads for ii ∈ 1:d
+            if A[ii] ≠ B[ii]
+                vals = ((box[ii].lo-A[ii])/(B[ii]-A[ii]), (box[ii].hi-A[ii])/(B[ii]-A[ii]));
+                low = minimum(vals);
+                high = maximum(vals);
+                ϕₜ = push!(ϕₜ, low..high); # store the range of permitted t values for A + t (B-A) in the iith component 
             end
         end
+        
         φₜ = intmultints(ϕₜ)
-        ## next we deal with the (C-A) direction (actually irrelevant for the current way the code is written)
-        ϕₛ = [] # here i should define an empty vector in a better way! i'm later going to fill it with Interval{Float64}s
-        if A ≠ C
-            @Threads.threads for ii ∈ 1:d
-                if A[ii] ≠ C[ii]
-                    vals = ((box[ii].lo-A[ii])/(C[ii]-A[ii]), (box[ii].hi-A[ii])/(C[ii]-A[ii]));
-                    low = minimum(vals);
-                    high = maximum(vals);
-                    ϕₛ = push!(ϕₛ, low..high); # store the range of permitted t values for A + s (C-A) in the ith component 
-                end
-            end
-        else # i.e. A = C 
-            @Threads.threads for ii ∈ 1:d
-                if A[ii] ≠ 0
-                    vals = (box[ii].lo/A[ii], box[ii].hi/A[ii]) 
-                    low = minimum(vals)
-                    high = maximum(vals)
-                    ϕₛ = push!(ϕₛ, low..high);
-                end
-            end
-        end
-        φₛ = intmultints(ϕₛ)
-        ## so right now we have φₜ, φₛ and we wish to subsequently loop over these 
+
+        ## so right now we have φₜ and we wish to subsequently loop over these 
+        
         U₁ = range(0, φₜ.lo; step = -step[1]);
         U₂ = range(0, φₜ.hi; step = step[2]);
-
-        #println([length(U₁), length(U₂)])
-
-        ## method one
-
-        # V = [[] for i ∈ 1:length(U₁), j ∈ 1:length(U₂)] 
-
-        # @showprogress for ii ∈ 1:length(U₁), jj ∈ 1:length(U₂)
-        #     v = vthreshold([U₁[ii], U₂[jj]], A, B, C, box)
-        #     V[ii,jj] = [[(U₁[ii], U₂[jj]), (v[1], v[2])], (v[2]-v[1])*(U₂[jj]-U₁[ii])]
-        # end
-
-        ## method two
 
         V = [[] for i ∈ 1:length(U₁), j ∈ 1:length(U₂)]
 
@@ -128,41 +88,136 @@ function plane(A, B, C, box; step::Vector = [0.1, 0.1])
 
         V[q][1];
 
-        ## method three
-
-        # V = [[] for i ∈ 1:length(U₁)*length(U₂)] 
-
-        # Us = [[U₁[1], u] for u ∈ U₂];
-        # for jj ∈ 2:length(U₂)
-        #     Us = vcat(Us, [[U₁[jj], u] for u ∈ U₂]);
-        # end
-
-        # @showprogress for ii ∈ 1:length(Us) 
-        #     v = vthreshold(Us[ii], A, B, C, box);
-        #     V[ii] = [[(Us[ii][1], Us[ii][2]), (v[1], v[2])], (v[2]-v[1])*(Us[ii][2]-Us[ii][1])]
-        # end
-        
-        ## method four
-
-        # Us = [[u₁, u₂] for u₁ ∈ U₁, u₂ ∈ U₂];
-
-        # Vs = vthreshold.(Us, Ref(A), Ref(B), Ref(C), Ref(box));
-
-        # V = [[[(Us[ii,jj][1], Us[ii,jj][2]), (Vs[ii,jj][1], Vs[ii,jj][2])], (Vs[ii,jj][2]-Vs[ii,jj][1])*(Us[ii,jj][2]-Us[ii,jj][1])] for ii ∈ 1:length(U₁), jj ∈ 1:length(U₂)];
-
-        ## implement parallel computing + broadcasting and check whether this is faster!!
-        ## compare with the matrix broadcasting method
-
-        # corners  = zeros(1,5); 
-        # @showprogress for u₁ ∈ U₁, u₂ ∈ U₂
-        #     for v₁ ∈ V₁, v₂ ∈ V₂
-        #         if [A .+ u₁*B + v₁.*C, A .+ u₁*B + v₂.*C, A .+ u₂*B + v₁.*C, A .+ u₂*B + v₂.*C] ⊆ box
-        #             # the corners of the plane (and hence the plane itself) are contained in the box
-        #             corners = vcat(corners, [u₁, u₂, v₁, v₂, (u₂-u₁)*(v₂-v₁)]')
-        #         end
-        #     end
-        # end
-        # corners = corners[2:end,:]
-    #return V
     end
 end
+
+# function plane(A, B, C, box; step::Vector = [0.1, 0.1])
+
+#     # box is a hyperrectangle in R^d         
+#     # here A is the starting vector, and B and C are vectors that determine the direction of the plane
+
+#     if A ∉ box 
+#         @warn("Please enter a starting vector that is contained within the prescribed box")
+#     elseif !all([length(B), length(C), length(box)] .== length(A))
+#         @warn("Please enter vectors A, B, C and a set 'box' that are of the same dimension")
+#     elseif A == zeros(length(A)) == B || A == zeros(length(A)) == C
+#         @warn("Please enter vectors such that neither A=B=0 or A=C=0 hold") 
+#     elseif normalise(B-A) == normalise(C-A)
+#         @warn("Please enter vectors B and C that are not collinear")
+#     elseif A == B || A == C || B = C
+#         @warn("Please enter distinct vectors A, B, and C.")
+#     else ## main code
+#         d = length(A); # the (shared) dimension that we are working within 
+#         ## firstly we deal in the (B-A) direction 
+#         ϕₜ = []; # here i should define an empty vector in a better way! i'm later going to fill it with Interval{Float64}s
+#         if A ≠ B
+#             @Threads.threads for ii ∈ 1:d
+#                 if A[ii] ≠ B[ii]
+#                     vals = ((box[ii].lo-A[ii])/(B[ii]-A[ii]), (box[ii].hi-A[ii])/(B[ii]-A[ii]));
+#                     low = minimum(vals);
+#                     high = maximum(vals);
+#                     ϕₜ = push!(ϕₜ, low..high); # store the range of permitted t values for A + t (B-A) in the iith component 
+#                 end
+#             end
+#         else # i.e. A = B, this is a separate interpretation!!! for when you are considering A+t*B but B=A
+#             @Threads.threads for ii ∈ 1:d
+#                 if A[ii] ≠ 0
+#                     vals = (box[ii].lo/A[ii], box[ii].hi/A[ii]) 
+#                     low = minimum(vals)
+#                     high = maximum(vals)
+#                     ϕₜ = push!(ϕₜ, low..high);
+#                 end
+#             end
+#         end
+#         φₜ = intmultints(ϕₜ)
+#         ## next we deal with the (C-A) direction (actually irrelevant for the current way the code is written)
+#         ϕₛ = [] # here i should define an empty vector in a better way! i'm later going to fill it with Interval{Float64}s
+#         if A ≠ C
+#             @Threads.threads for ii ∈ 1:d
+#                 if A[ii] ≠ C[ii]
+#                     vals = ((box[ii].lo-A[ii])/(C[ii]-A[ii]), (box[ii].hi-A[ii])/(C[ii]-A[ii]));
+#                     low = minimum(vals);
+#                     high = maximum(vals);
+#                     ϕₛ = push!(ϕₛ, low..high); # store the range of permitted t values for A + s (C-A) in the ith component 
+#                 end
+#             end
+#         else # i.e. A = C 
+#             @Threads.threads for ii ∈ 1:d
+#                 if A[ii] ≠ 0
+#                     vals = (box[ii].lo/A[ii], box[ii].hi/A[ii]) 
+#                     low = minimum(vals)
+#                     high = maximum(vals)
+#                     ϕₛ = push!(ϕₛ, low..high);
+#                 end
+#             end
+#         end
+#         φₛ = intmultints(ϕₛ)
+#         ## so right now we have φₜ, φₛ and we wish to subsequently loop over these 
+#         U₁ = range(0, φₜ.lo; step = -step[1]);
+#         U₂ = range(0, φₜ.hi; step = step[2]);
+
+#         #println([length(U₁), length(U₂)])
+
+#         ## method one
+
+#         # V = [[] for i ∈ 1:length(U₁), j ∈ 1:length(U₂)] 
+
+#         # @showprogress for ii ∈ 1:length(U₁), jj ∈ 1:length(U₂)
+#         #     v = vthreshold([U₁[ii], U₂[jj]], A, B, C, box)
+#         #     V[ii,jj] = [[(U₁[ii], U₂[jj]), (v[1], v[2])], (v[2]-v[1])*(U₂[jj]-U₁[ii])]
+#         # end
+
+#         ## method two
+
+#         V = [[] for i ∈ 1:length(U₁), j ∈ 1:length(U₂)]
+
+#         @Threads.threads for ii ∈ 1:length(U₁)
+#             us = [[U₁[ii], u] for u ∈ U₂]; # the candidate for umin and umax
+#             vs = vthreshold.(us, Ref(A), Ref(B), Ref(C), Ref(box));
+#             V[ii,:] = [[[(U₁[ii], U₂[jj]), (vs[jj][1], vs[jj][2])], (vs[jj][2] - vs[jj][1])*(U₂[jj] - U₁[ii])] for jj ∈ 1:length(U₂)]
+#         end
+
+#         W = [V[ii,jj][2] for ii ∈ 1:size(V,1), jj ∈ 1:size(V,2)];
+
+#         q = findmax(W)[2];
+
+#         V[q][1];
+
+#         ## method three
+
+#         # V = [[] for i ∈ 1:length(U₁)*length(U₂)] 
+
+#         # Us = [[U₁[1], u] for u ∈ U₂];
+#         # for jj ∈ 2:length(U₂)
+#         #     Us = vcat(Us, [[U₁[jj], u] for u ∈ U₂]);
+#         # end
+
+#         # @showprogress for ii ∈ 1:length(Us) 
+#         #     v = vthreshold(Us[ii], A, B, C, box);
+#         #     V[ii] = [[(Us[ii][1], Us[ii][2]), (v[1], v[2])], (v[2]-v[1])*(Us[ii][2]-Us[ii][1])]
+#         # end
+        
+#         ## method four
+
+#         # Us = [[u₁, u₂] for u₁ ∈ U₁, u₂ ∈ U₂];
+
+#         # Vs = vthreshold.(Us, Ref(A), Ref(B), Ref(C), Ref(box));
+
+#         # V = [[[(Us[ii,jj][1], Us[ii,jj][2]), (Vs[ii,jj][1], Vs[ii,jj][2])], (Vs[ii,jj][2]-Vs[ii,jj][1])*(Us[ii,jj][2]-Us[ii,jj][1])] for ii ∈ 1:length(U₁), jj ∈ 1:length(U₂)];
+
+#         ## implement parallel computing + broadcasting and check whether this is faster!!
+#         ## compare with the matrix broadcasting method
+
+#         # corners  = zeros(1,5); 
+#         # @showprogress for u₁ ∈ U₁, u₂ ∈ U₂
+#         #     for v₁ ∈ V₁, v₂ ∈ V₂
+#         #         if [A .+ u₁*B + v₁.*C, A .+ u₁*B + v₂.*C, A .+ u₂*B + v₁.*C, A .+ u₂*B + v₂.*C] ⊆ box
+#         #             # the corners of the plane (and hence the plane itself) are contained in the box
+#         #             corners = vcat(corners, [u₁, u₂, v₁, v₂, (u₂-u₁)*(v₂-v₁)]')
+#         #         end
+#         #     end
+#         # end
+#         # corners = corners[2:end,:]
+#     #return V
+#     end
+# end
