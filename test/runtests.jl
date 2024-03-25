787efba3e96a26292c23fbfcbd7d9c57e0f92ad7
oameye <orjan.ameye@hotmail.com>
2024-03-25 09:03:53 +0100

    setup test structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,17 @@
 using CriticalTransitions
 using Test
 
-@testset "OuMAM" begin
-    ou = StochSystem((u,p,t)->-u,[],1,1.0);
-    x0 = -1
-    xT = 2.
-    T = 10.
-    N = 51
-    t = range(0,T,N)
-    inst_mam = min_action_method(ou,[x0], [xT], N,T,showprogress=false);
-    inst_sol = ((xT - x0*exp(-T))*exp.(t) .+ (x0*exp(T) - xT)*exp.(-t))/(exp(T) - exp(-T));
-    @test maximum(abs.(inst_mam' .- inst_sol)) < 0.1
+using Random
+const SEED = 0xd8e5d8df
+Random.seed!(SEED)
+
+files = [
+    "gMAM.jl",
+    "stochsystem.jl",
+    "MAM.jl"
+]
+
+for file in files
+    include(file)
+    printstyled(file * ":    OK\n"; color = :green)
 end
