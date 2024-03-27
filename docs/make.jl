2e11371608f4ad12a391a305175ee66cdc8c047b
oameye <orjan.ameye@hotmail.com>
2024-03-27 08:36:31 +0100

    try some stuff :p

--- docs/make.jl
+++ docs/make.jl
@@ -9,10 +9,15 @@ bib = CitationBibliography(
 
 makedocs(;
     sitename="CriticalTransitions.jl",
+    repo = Documenter.Remotes.GitHub("JuliaDynamics", "CriticalTransitions.jl"),
+    # remote = Documenter.Remotes.GitHub("JuliaDynamics", "CriticalTransitions.jl"),
+    modules=[CriticalTransitions],
     doctest=false,
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true",
-        mathengine = Documenter.MathJax2()),
-    warnonly = [:doctest, :missing_docs],
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        mathengine = Documenter.MathJax2()
+        ),
+    warnonly = [:doctest, :missing_docs,:cross_references],
     pages=Any[
         "Home" => "index.md",
         "Quickstart" => "quickstart.md",
