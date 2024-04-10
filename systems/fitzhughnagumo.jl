653c1a77d55e67a09e0b03bcf608acd405584a8c
oameye <orjan.ameye@hotmail.com>
2024-04-10 20:42:29 +0200

    rerun Documenter

--- systems/fitzhughnagumo.jl
+++ systems/fitzhughnagumo.jl
@@ -3,7 +3,6 @@ Dynamical systems specification file
 """
 
 # fitzhugh_nagumo model
-
 """
     fitzhugh_nagumo!(du, u, p, t)
 In-place definition of the FitzHugh-Nagumo system.
@@ -41,31 +40,31 @@ FitzHughNagumo!(u,p,t) = fitzhugh_nagumo!(u,p,t)
 
 """
     fhn_ϵσ(ϵ,σ)
-A shortcut command for returning a StochSystem of the FitzHugh Nagumo system in a default setup with additive isotropic noise. 
-    
+A shortcut command for returning a StochSystem of the FitzHugh Nagumo system in a default setup with additive isotropic noise.
+
 This setup fixes the parameters β = 3, α =  γ = κ = 1, I = 0 and leaves the value of the time-scale parameter ϵ as a function argument. The prescribed noise process is additive and isotropic: the variables are peturbed by independently drawn identical Gaussian white noise realisations, with standard deviation σ (the other function argument).
 """
-function fhn_ϵσ(ϵ, σ) # a convenient two-parameter version of the FitzHugh Nagumo system 
+function fhn_ϵσ(ϵ, σ) # a convenient two-parameter version of the FitzHugh Nagumo system
     # defining the StochSystem
     f(u,p,t) = fitzhugh_nagumo(u,p,t);
     β = 3; α = γ = κ = 1; I = 0; # standard parameters without ϵ (time-scale separation parameter)
     pf_wo_ϵ = [β, α, γ, κ, I]; # parameter vector without ϵ
     dim = 2;
     g = idfunc;
-    pg = nothing; 
+    pg = nothing;
     Σ = [1 0; 0 1];
     process = "WhiteGauss";
     StochSystem(f, vcat([ϵ], pf_wo_ϵ), dim, σ, g, pg, Σ, process)
 end;
 
-function fhn_ϵσ_backward(ϵ, σ) # a convenient two-parameter version of the FitzHugh Nagumo system 
+function fhn_ϵσ_backward(ϵ, σ) # a convenient two-parameter version of the FitzHugh Nagumo system
     # defining the StochSystem
     f(u,p,t) = -fitzhugh_nagumo(u,p,t);
     β = 3; α = γ = κ = 1; I = 0; # standard parameters without ϵ (time-scale separation parameter)
     pf_wo_ϵ = [β, α, γ, κ, I]; # parameter vector without ϵ
     dim = 2;
     g = idfunc;
-    pg = nothing; 
+    pg = nothing;
     Σ = [1 0; 0 1];
     process = "WhiteGauss";
     StochSystem(f, vcat([ϵ], pf_wo_ϵ), dim, σ, g, pg, Σ, process)
