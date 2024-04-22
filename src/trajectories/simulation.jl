e46779c4057fb1cae26701003125777b22c953b8
Orjan Ameye <orjan.ameye@hotmail.com>
2024-04-22 22:56:14 +0200

    comment unneeded stuff

--- src/trajectories/simulation.jl
+++ src/trajectories/simulation.jl
@@ -1,62 +1,62 @@
-include("../noiseprocesses/stochprocess.jl")
+# include("../noiseprocesses/stochprocess.jl")
 
-"""
-    simulate(sys::StochSystem, init::State; kwargs...)
-Simulates the StochSystem `sys` forward in time, starting at initial condition `init`.
+# """
+#     simulate(sys::CoupledSDES, init::State; kwargs...)
+# Simulates the StochSystem `sys` forward in time, starting at initial condition `init`.
 
-This function uses the [`SDEProblem`](https://diffeq.sciml.ai/stable/types/sde_types/#SciMLBase.SDEProblem) functionality of `DifferentialEquations.jl`.
+# This function uses the [`SDEProblem`](https://diffeq.sciml.ai/stable/types/sde_types/#SciMLBase.SDEProblem) functionality of `DifferentialEquations.jl`.
 
-## Keyword arguments
-* `dt=0.01`: time step of integration
-* `tmax=1e3`: total time of simulation
-* `solver=EM()`: [SDE solver](https://docs.sciml.ai/DiffEqDocs/stable/solvers/sde_solve/#sde_solve). Defaults to Euler-Mayurama
-* `callback=nothing`: callback condition
-* `progress=true`: shows a progress bar during simulation
-* `kwargs...`: keyword arguments for [`solve(ODEProblem)`](https://docs.sciml.ai/DiffEqDocs/stable/basics/common_solver_opts/#solver_options)
+# ## Keyword arguments
+# * `dt=0.01`: time step of integration
+# * `tmax=1e3`: total time of simulation
+# * `solver=EM()`: [SDE solver](https://docs.sciml.ai/DiffEqDocs/stable/solvers/sde_solve/#sde_solve). Defaults to Euler-Mayurama
+# * `callback=nothing`: callback condition
+# * `progress=true`: shows a progress bar during simulation
+# * `kwargs...`: keyword arguments for [`solve(ODEProblem)`](https://docs.sciml.ai/DiffEqDocs/stable/basics/common_solver_opts/#solver_options)
 
-For more info, see [`SDEProblem`](https://diffeq.sciml.ai/stable/types/sde_types/#SciMLBase.SDEProblem).
+# For more info, see [`SDEProblem`](https://diffeq.sciml.ai/stable/types/sde_types/#SciMLBase.SDEProblem).
 
-> Warning: This function has only been tested for the `EM()` solver and out-of-place `SDEFunction`s.
-"""
-function simulate(sys::StochSystem, init::State;
-    dt=0.01,
-    tmax=1e3,
-    solver=EM(),
-    callback=nothing,
-    showprogress=false,
-    iip=is_iip(sys.f),
-    kwargs...)
+# > Warning: This function has only been tested for the `EM()` solver and out-of-place `SDEFunction`s.
+# """
+# function simulate(sys::StochSystem, init::State;
+#     dt=0.01,
+#     tmax=1e3,
+#     solver=EM(),
+#     callback=nothing,
+#     showprogress=false,
+#     iip=is_iip(sys.f),
+#     kwargs...)
 
-    prob = SDEProblem{iip}(sys.f, σg(sys), init, (0, tmax), p(sys), noise=stochprocess(sys))
-    solve(prob, solver; dt=dt, callback=callback, progress=showprogress, kwargs...)
-end;
+#     prob = SDEProblem{iip}(sys.f, σg(sys), init, (0, tmax), p(sys), noise=stochprocess(sys))
+#     solve(prob, solver; dt=dt, callback=callback, progress=showprogress, kwargs...)
+# end;
 
-"""
-    relax(sys::StochSystem, init::State; kwargs...)
-Simulates the deterministic dynamics of StochSystem `sys` in time, starting at initial condition `init`.
+# """
+#     relax(sys::StochSystem, init::State; kwargs...)
+# Simulates the deterministic dynamics of StochSystem `sys` in time, starting at initial condition `init`.
 
-This function integrates `sys.f` forward in time, using the [`ODEProblem`](https://diffeq.sciml.ai/stable/types/ode_types/#SciMLBase.ODEProblem) functionality of `DifferentialEquations.jl`. Thus, `relax` is identical to [`simulate`](@ref) when setting the noise strength `sys.σ = 0`.
+# This function integrates `sys.f` forward in time, using the [`ODEProblem`](https://diffeq.sciml.ai/stable/types/ode_types/#SciMLBase.ODEProblem) functionality of `DifferentialEquations.jl`. Thus, `relax` is identical to [`simulate`](@ref) when setting the noise strength `sys.σ = 0`.
 
-## Keyword arguments
-* `dt=0.01`: time step of integration
-* `tmax=1e3`: total time of simulation
-* `solver=Euler()`: [ODE solver](https://docs.sciml.ai/DiffEqDocs/stable/solvers/ode_solve/#ode_solve). Defaults to explicit forward Euler
-* `callback=nothing`: callback condition
-* `kwargs...`: keyword arguments for [`solve(ODEProblem)`](https://docs.sciml.ai/DiffEqDocs/stable/basics/common_solver_opts/#solver_options)
+# ## Keyword arguments
+# * `dt=0.01`: time step of integration
+# * `tmax=1e3`: total time of simulation
+# * `solver=Euler()`: [ODE solver](https://docs.sciml.ai/DiffEqDocs/stable/solvers/ode_solve/#ode_solve). Defaults to explicit forward Euler
+# * `callback=nothing`: callback condition
+# * `kwargs...`: keyword arguments for [`solve(ODEProblem)`](https://docs.sciml.ai/DiffEqDocs/stable/basics/common_solver_opts/#solver_options)
 
-For more info, see [`ODEProblem`](https://diffeq.sciml.ai/stable/types/ode_types/#SciMLBase.ODEProblem).
-For stochastic integration, see [`simulate`](@ref).
+# For more info, see [`ODEProblem`](https://diffeq.sciml.ai/stable/types/ode_types/#SciMLBase.ODEProblem).
+# For stochastic integration, see [`simulate`](@ref).
 
-> Warning: This function has only been tested for the `Euler()` solver.
-"""
-function relax(sys::StochSystem, init::State;
-    dt=0.01,
-    tmax=1e3,
-    solver=Euler(),
-    callback=nothing,
-    iip=is_iip(sys.f),
-    kwargs...)
+# > Warning: This function has only been tested for the `Euler()` solver.
+# """
+# function relax(sys::StochSystem, init::State;
+#     dt=0.01,
+#     tmax=1e3,
+#     solver=Euler(),
+#     callback=nothing,
+#     iip=is_iip(sys.f),
+#     kwargs...)
 
-    prob = ODEProblem{iip}(sys.f, init, (0, tmax), p(sys))
-    solve(prob, solver; dt=dt, callback=callback, kwargs...)
-end;
+#     prob = ODEProblem{iip}(sys.f, init, (0, tmax), p(sys))
+#     solve(prob, solver; dt=dt, callback=callback, kwargs...)
+# end;
