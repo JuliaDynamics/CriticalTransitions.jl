8482c5598b656f413ca6a5ec6c9551a18ff09a7b
Reyk Börner <reyk.boerner@reading.ac.uk>
2024-07-23 08:18:45 +0100

    change by JuliaFormatter
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/CoupledSDEs.jl
+++ src/CoupledSDEs.jl
@@ -187,8 +187,10 @@ end
 function additional_details(ds::CoupledSDEs)
     solver, remaining = _decompose_into_solver_and_remaining(ds.diffeq)
     return [
-        "SDE solver" => string(nameof(typeof(solver))), "SDE kwargs" => remaining,
-        "Noise strength" => ds.noise_strength]
+        "SDE solver" => string(nameof(typeof(solver))),
+        "SDE kwargs" => remaining,
+        "Noise strength" => ds.noise_strength,
+    ]
 end
 
 ###########################################################################################
