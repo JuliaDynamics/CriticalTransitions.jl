202b428facd8762d0d2f42456eedfc14906c8ed2
Orjan Ameye <orjan.ameye@hotmail.com>
2024-11-21 15:14:07 +0100

    refactor: move code quality tests to a separate file

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,32 +8,8 @@ Random.seed!(SEED)
 
 using CriticalTransitions.CTLibrary: fitzhugh_nagumo
 
-@testset "Code quality" begin
-    using ExplicitImports, Aqua
-    ignore_deps = [:Random, :LinearAlgebra, :Printf, :Test, :Pkg]
-
-    @test check_no_stale_explicit_imports(CriticalTransitions) == nothing
-    @test check_all_explicit_imports_via_owners(CriticalTransitions) == nothing
-    Aqua.test_ambiguities(CriticalTransitions)
-    Aqua.test_all(
-        CriticalTransitions;
-        deps_compat=(
-            ignore=ignore_deps,
-            check_extras=(ignore=ignore_deps,),
-            check_weakdeps=(ignore=ignore_deps,),
-        ),
-        piracies=(
-            treat_as_own=[
-                CriticalTransitions.DynamicalSystemsBase.SciMLBase.AbstractSDEIntegrator
-            ],
-        ),
-        ambiguities=false,
-    )
-end
-
-@testset "Code linting" begin
-    using JET
-    JET.test_package(CriticalTransitions; target_defined_modules=true)
+@testset "code quality" begin
+    include("code_quality.jl")
 end
 
 @testset "CoupledSDEs" begin
