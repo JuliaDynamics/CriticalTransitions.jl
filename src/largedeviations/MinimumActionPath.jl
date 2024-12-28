6c7969cb328b5d7d15f61b59d10ce3b784adcb00
Orjan Ameye <orjan.ameye@hotmail.com>
2024-12-28 16:00:20 +0100

    add lost docstring

--- src/largedeviations/MinimumActionPath.jl
+++ src/largedeviations/MinimumActionPath.jl
@@ -1,3 +1,18 @@
+"""
+    MinimumActionPath{T,Phis,Ahis,Lambda,PV,GPV}
+
+The minimum action path between two points in phase space.
+
+# Fields
+- `path::Matrix{T}`: The path matrix.
+- `action::T`: The action value associated to the path.
+- `path_history::Phis`: The history of paths in the optimisation algorithm (optional).
+- `action_history::Ahis`: The history of action of the paths in the ptimisation algorithm (optional).
+- `λ::Lambda`: The Lagrange multiplier parameter for the maximum likelihood path.
+- `generalized_momentum::GPV`: The generalized momentum of the phase space variables (optional).
+- `path_velocity::PV`: The path velocity (optional).
+"""
+
 mutable struct MinimumActionPath{D,T,V,Phis,Ahis,Lambda,PV,GPV}
     path::StateSpaceSet{D,T,V}
     action::T
