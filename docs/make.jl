b7ab4ab14abcdf11dee36f585edad7932c6c30e7
Orjan Ameye <orjan.ameye@hotmail.com>
2024-12-31 11:56:29 +0000

    turn off draft in make docs file

--- docs/make.jl
+++ docs/make.jl
@@ -61,7 +61,7 @@ makedocs(;
     format=Documenter.HTML(; html_options...),
     warnonly=[:missing_docs, :linkcheck, :cross_references],
     plugins=[bib, links],
-    draft=true,
+    draft=false,
 )
 
 deploydocs(; repo="github.com/JuliaDynamics/CriticalTransitions.jl.git", push_preview=true)
