e5ced08ed648c4a0421dfd4897b3e943fbf7abfe
Orjan Ameye <orjan.ameye@hotmail.com>
2025-01-01 14:14:53 +0100

    fix typo

--- test/trajectories/transition.jl
+++ test/trajectories/transition.jl
@@ -18,13 +18,13 @@
     @test norm(tr[end, :] - fp2) < 0.1
 
     ensemble = transitions(sys, fp1, fp2, 10)
-    @test approx(ensemble.success_rate, 0.833; atol=1e-2) ||
-        approx(ensemble.success_rate, 0.909; atol=1e-2)
-    @test approx(ensemble.success_rate, 5.213; atol=1e-2) ||
-        approx(ensemble.success_rate, 5.6512; atol=1e-2)
+    @test isapprox(ensemble.success_rate, 0.833; atol=1e-2) ||
+        isapprox(ensemble.success_rate, 0.909; atol=1e-2)
+    @test isapprox(ensemble.success_rate, 5.213; atol=1e-2) ||
+        isapprox(ensemble.success_rate, 5.6512; atol=1e-2)
     # SEED is different on github
     # SEED doesn;t work on github
     @test length(ensemble.times) == 10
-    @test approx(ensemble.residence_time, 346.5424; atol=1e-2) ||
-        approx(ensemble.residence_time, 177.70; atol=1e-2)
+    @test isapprox(ensemble.residence_time, 346.5424; atol=1e-2) ||
+        isapprox(ensemble.residence_time, 177.70; atol=1e-2)
 end
