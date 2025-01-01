52c3cca14e772376191f29c73860dde35f9414a4
Orjan Ameye <orjan.ameye@hotmail.com>
2025-01-01 09:54:22 +0100

    remove unnecessary seed! import from Random

--- src/CriticalTransitions.jl
+++ src/CriticalTransitions.jl
@@ -6,7 +6,7 @@ using LinearAlgebra: LinearAlgebra, I, norm, dot, tr
 using StaticArrays: StaticArrays, SVector
 using SparseArrays: spdiagm
 using DataStructures: CircularBuffer
-using Random: Random, seed!
+using Random: Random
 
 # Core
 using SciMLBase: EnsembleThreads, DiscreteCallback, remake, terminate!
