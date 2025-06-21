4cc0b53bc1b0b047cef70726365f25b3759a8e66
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-21 10:55:19 +0200

    test: cleanup code quality tests

--- test/code_quality.jl
+++ test/code_quality.jl
@@ -1,17 +1,9 @@
-@testset "Code quality" begin
-    using ExplicitImports, Aqua
-    ignore_deps = [:Random, :LinearAlgebra, :Printf, :Test, :Pkg]
+@testset "best practices" begin
+    using Aqua
 
-    @test isnothing(check_no_stale_explicit_imports(CriticalTransitions))
-    @test isnothing(check_all_explicit_imports_via_owners(CriticalTransitions))
     Aqua.test_ambiguities(CriticalTransitions)
     Aqua.test_all(
         CriticalTransitions;
-        deps_compat=(
-            ignore=ignore_deps,
-            check_extras=(ignore=ignore_deps,),
-            check_weakdeps=(ignore=ignore_deps,),
-        ),
         piracies=(
             treat_as_own=[
                 CriticalTransitions.DynamicalSystemsBase.SciMLBase.AbstractSDEIntegrator,
@@ -22,7 +14,22 @@
     )
 end
 
-@testset "Code linting" begin
-    using JET
-    JET.test_package(CriticalTransitions; target_defined_modules=true)
+@testset "ExplicitImports" begin
+    using ExplicitImports
+
+    # @test check_no_implicit_imports(CriticalTransitions) == nothing
+    @test check_all_explicit_imports_via_owners(CriticalTransitions) == nothing
+    # @test check_all_explicit_imports_are_public(CriticalTransitions) == nothing
+    @test check_no_stale_explicit_imports(CriticalTransitions) == nothing
+    # @test check_all_qualified_accesses_via_owners(CriticalTransitions) == nothing
+    # @test check_all_qualified_accesses_are_public(KeldyshContraction) == nothing
+    # @test check_no_self_qualified_accesses(CriticalTransitions) == nothing
+end
+
+
+if isempty(VERSION.prerelease)
+    @testset "Code linting" begin
+        using JET
+        JET.test_package(CriticalTransitions; target_defined_modules=true)
+    end
 end
