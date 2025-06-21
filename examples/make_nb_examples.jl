5c2bf6fb9317401cc24e99a162bd75be305efc2c
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-21 11:07:16 +0200

    docs: sync example folder with docs using Literate (#175)

---
+++ examples/make_nb_examples.jl
@@ -0,0 +1,15 @@
+using Literate
+
+using KeldyshContraction
+
+### Process examples
+# Always rerun examples
+const EXAMPLES_IN = @__DIR__
+const OUTPUT_NB_DIR = @__DIR__
+
+examples = filter!(file -> file[(end - 2):end] == ".jl", readdir(EXAMPLES_IN; join=true))
+filter!(file -> !contains(file, "make_nb_examples"), examples)
+
+for example in examples
+    Literate.notebook(example, OUTPUT_NB_DIR; documenter=false, execute=true)
+end
