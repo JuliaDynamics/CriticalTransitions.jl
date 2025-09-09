00ae0c9f03cce5175af148bbbd06b5b10a2ae8d5
raphael-roemer <raphael-roemer@t-online.de>
2025-09-09 14:51:32 +0100

    added documentation of the example

--- examples/RateSys3rdIterTest.jl
+++ examples/RateSys3rdIterTest.jl
@@ -17,10 +17,26 @@ p_auto = [0.];
 auto_sys = CoupledODEs(f,x0,p_auto);
 
 
-# Setting up the parameter ramping:
+# Set up the parameter ramping:
+# 1) First specify 
+#       - the index pidx of the parameter of the autonomous system that you would like to apply the shift to
+# 2) Then the shape of the parameter shift you would like to consider by giving
+#       - a monotonic function p(t) describing the shape of the shift
+#       - an interval [section_start, section_end] over which p(t) should be considered
+#
+# 2) Specify how you would like to use this section p([section_start, section_end]) to shift the pidx'th parameter 
+#    of the autonomous system. This is done by specifying:
+#       - a window_start, i.e. the time when the parameter shift should start (before this, the system is autonomous)
+#       - a window_length over which p([section_start, section_end]) is 
+#           spread out (for window_length > section_end - section_start) or 
+#           squeezed into (for window_length < section_end - section_start)
+#       - an amplitude dp. Then p(t) is automatically scaled s.t. p(section_end) - p(section_start) = dp
+
+
 pidx=1;                 # Index of the parameter that is made nonautonomous 
 p(t) = tanh(t);         # Function that describes the parameter shift
-# plot the function to see which part of it we would like to consider for the parameter shift
+
+# plot p(t) to see which part of it we would like to consider
 p_plotvals = [p(t) for t in -10.0:0.1:10.0];
 figp = Figure(); 
 axsp = Axis(figp[1,1],xlabel="t",ylabel=L"p");
@@ -28,32 +44,33 @@ lines!(axsp,-10.0:0.1:10.0,p_plotvals,linewidth=2,label=L"p(t)");
 axislegend(axsp,position=:rc,labelsize=10);
 figp
 
-# define which section of the function we would like to consider for the parameter shift
+# now specify the section of p(t) you would like to consider
 section_start = -100;    # start time of the section of the tanh we want to consider
 section_end = 100;       # start time of the section of the tanh we want to consider
 
-# Now set the amplitude of the parameter ramping (i.e. it will go from p0 to )
-dp=3;                    # amplitude of the paramter ramping
+window_start = -50.;    # time when the parameter shift should start (before this, the system is autonomous)
 
 # for this section of the tanh the critical window_length is approximately 100 for p0 = 0 and dp = 3
-# hence for window_length < 100, the trajectory tips for large enough simulation time
-# hence for window_length > 100, the trajectory track to -4 for large enough simulation time
+# hence for window_length < 100, the trajectory tips
+# and for window_length > 100, the trajectory tracks to -4
+# So choose one of the following two lines:
+# window_length = 95; # tips!
+window_length = 105; # tracks!
 
-window_start = -50.;
-window_length = 95; # tips!
+# Now set the amplitude of the parameter ramping (i.e. it will go from p0 to p0+dp)
+dp=3;                   
 
-# window_start = -50.;
-# window_length = 105; # tracks!
-
+# set up the nonauto_sys by combining all this information in a RateConfig
 rc = CriticalTransitions.RateConfig(pidx,p,section_start, section_end, window_start, window_length, dp);
-
 t0 = window_start - 20.0;      # initial time of the system
 nonauto_sys = apply_ramping(auto_sys, rc, t0);
 
-T = window_length + 40.0;        # total simulation time
-auto_traj = trajectory(auto_sys, T, x0);
+# Compute trajectories
+T = window_length + 40.0;        # simulation time
+auto_traj = trajectory(auto_sys, T, x0);   
 nonauto_traj = trajectory(nonauto_sys, T, x0);
 
+# Plot trajectories
 fig = Figure(); 
 axs = Axis(fig[1,1],xlabel="t",ylabel="x");
 lines!(axs,t0.+auto_traj[2],auto_traj[1][:,1],linewidth=2,label=L"\text{Autonomous system}");
