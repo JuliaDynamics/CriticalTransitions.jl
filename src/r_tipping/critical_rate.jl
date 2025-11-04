304e2cace32419a00493b707f924d2a51f791fcf
reykboerner <r.borner@uu.nl>
2025-11-04 17:36:36 +0100

    update moving_sinks, fix spelling and bump version

--- src/r_tipping/critical_rate.jl
+++ src/r_tipping/critical_rate.jl
@@ -3,10 +3,17 @@ using LinearAlgebra: norm
 ## the following function integrates the nonautonomous system created by the ContinuousTimeDynamicalSystem ds and RateProtocol rp, with rate-parameter r 
 ## the initial condition is e_start at time t_start
 ## the system is integrated for T = t_end-t_start time units and the trajectory position at time T is then compared with the location of e_end 
-## if the trajectory position at time T is within a neighbourhood of radius rad of e_end, the function returns true, and false otherwise  
+## if the trajectory position at time T is within a neighborhood of radius rad of e_end, the function returns true, and false otherwise  
 
 function end_point_tracking(
-    ds::ContinuousTimeDynamicalSystem, rp::RateProtocol, r::Float64, e_start::Vector, t_start::Float64, e_end::Vector, t_end::Float64, rad::Float64
+    ds::ContinuousTimeDynamicalSystem,
+    rp::RateProtocol,
+    r::Float64,
+    e_start::Vector,
+    t_start::Float64,
+    e_end::Vector,
+    t_end::Float64,
+    rad::Float64,
 )
 
     # note that should ensure that Δt << dλ(rt)/dt
@@ -17,18 +24,20 @@ function end_point_tracking(
     nonauto_sys = apply_ramping(ds, rp, t_start) # create the nonautonomous system
     traj = trajectory(nonauto_sys, T, e_start) # simulate the nonautonomous system
 
-    return norm(traj[1][end,:]-e_end) < rad 
-
+    return norm(traj[1][end, :]-e_end) < rad
 end
 
 # the following function finds via brute force an interval [a,b] satisfying b-a ≤ tol satisfying f(a)*f(b) == false for a function f returning boolean values 
 # i.e. a small interval [a,b] for which the value of the function f evaluated at the points a and b returns both true and false  
 
-function binary_bisection(f::Function, a::Float64, b::Float64;
-    tol::Float64=1e-12, maxiter::Int64=10000)
-    a < b || error("Please enter a, b such that a < b.") 
+function binary_bisection(
+    f::Function, a::Float64, b::Float64; tol::Float64=1e-12, maxiter::Int64=10000
+)
+    a < b || error("Please enter a, b such that a < b.")
     fa = f(a)
-    fa*f(b) == false || error("The function does not satisfy the initial requirement that f(a)*f(b) == false")
+    fa*f(b) == false || error(
+        "The function does not satisfy the initial requirement that f(a)*f(b) == false"
+    )
     ii = 0
     local c
     while b-a > tol
@@ -37,13 +46,13 @@ function binary_bisection(f::Function, a::Float64, b::Float64;
         c = (a+b)/2
         fc = f(c)
         if fa*fc == true # reduce to the shrunken interval [c,b], since we must have f(c)*f(b) == false
-            a = c  
-            fa = fc 
+            a = c
+            fa = fc
         else # reduce to the shrunken interval [a,c], since we have f(a)*f(c) == false
-            b = c  
+            b = c
         end
     end
-    return [a,b]
+    return [a, b]
 end;
 
 ## the following function aims to find a critical value for the rate-parameter between successful and non-successful "end-point tracking" 
@@ -57,91 +66,107 @@ end;
 ##...rather than just a vector of vectors containing all the (non-grouped) equilibria found for a range of frozen-in times  
 
 ## there are check for ensuring that e_start and e_end are indeed stable equilibria of the frozen-in systems attained at times t_start and t_end respectively
-## there are also checks that the system succesfully end-point tracks for r = rmin and fails to end-point track for r = rmax (or vice versa, for exotic cases)
+## there are also checks that the system successfully end-point tracks for r = rmin and fails to end-point track for r = rmax (or vice versa, for exotic cases)
 
 ## the function ultimately passes the end_point_tracking function to the binary_bisection function to find a critical rate for end-point tracking
- 
 
 function critical_rate(
-    ds::ContinuousTimeDynamicalSystem, rp::RateProtocol, e_start::Vector, t_start::Float64, e_end::Vector, t_end::Float64;  
-    rmin::Float64 = 1.e-2, 
-    rmax::Float64 = 1.e2,
-    rad::Float64 = 1.e-1,
-    tol::Float64 = 1.e-3,
-    maxiter::Int64 = Int64(1.e4)
+    ds::ContinuousTimeDynamicalSystem,
+    rp::RateProtocol,
+    e_start::Vector,
+    t_start::Float64,
+    e_end::Vector,
+    t_end::Float64;
+    rmin::Float64=1.e-2,
+    rmax::Float64=1.e2,
+    rad::Float64=1.e-1,
+    tol::Float64=1.e-3,
+    maxiter::Int64=Int64(1.e4),
 )
-
-    rmin < rmax || error("Please enter rmin, rmax such that rmin < rmax.") 
+    rmin < rmax || error("Please enter rmin, rmax such that rmin < rmax.")
 
     ##-----check no.1-----## 
 
     ## that e_start is a stable equilibrium of the frozen-in system attained at t = t_start
 
-    λ_mod  = rp.λ
+    λ_mod = rp.λ
 
-    rp.λ = (p,τ) -> λ_mod(p,τ+t_start)
+    rp.λ = (p, τ) -> λ_mod(p, τ+t_start)
     rate_sys_start = apply_ramping(ds, rp, rp.t_start)
-    box_start = [interval(x-0.1,x+0.1) for x ∈ e_start]
+    box_start = [interval(x-0.1, x+0.1) for x in e_start]
     fp_start, ~, stab_start = fixedpoints(rate_sys_start, box_start)
-    if any(e -> isapprox(e,e_start;atol=1e-4),fp_start)
-        idx = findmin([norm(e-e_start) for e ∈ fp_start])[2] # the index in fp_start of the fixed point which is approximately equal to e_end
-        stab_start[idx] || @warn("The vector e_start = $(e_start) does not describe a stable equilibrium of the frozen-in system attained at t = t_start = $(t_start).")
+    if any(e -> isapprox(e, e_start; atol=1e-4), fp_start)
+        idx = findmin([norm(e-e_start) for e in fp_start])[2] # the index in fp_start of the fixed point which is approximately equal to e_end
+        stab_start[idx] || @warn(
+            "The vector e_start = $(e_start) does not describe a stable equilibrium of the frozen-in system attained at t = t_start = $(t_start)."
+        )
     else
-        @warn("The vector e_start = $(e_start) does not describe a stable equilibrium of the frozen-in system attained at t = t_start = $(t_start).")
+        @warn(
+            "The vector e_start = $(e_start) does not describe a stable equilibrium of the frozen-in system attained at t = t_start = $(t_start)."
+        )
     end
 
     ##-----check no.2-----##
-    
+
     ## that e_end is a stable equilibrium of the frozen-in system attained at t = t_end
 
-    rp.λ = (p,τ) -> λ_mod(p,τ+t_end)
+    rp.λ = (p, τ) -> λ_mod(p, τ+t_end)
     rate_sys_end = apply_ramping(ds, rp, rp.t_start)
-    box_end = [interval(x-0.1,x+0.1) for x ∈ e_end]
+    box_end = [interval(x-0.1, x+0.1) for x in e_end]
     fp_end, ~, stab_end = fixedpoints(rate_sys_end, box_end)
-    if any(e -> isapprox(e,e_end;atol=1e-4),fp_end)
-        idx = findmin([norm(e-e_end) for e ∈ fp_end])[2] # the index in fp_end of the fixed point which is approximately equal to e_end
-        stab_end[idx] || @warn("The vector e_end = $(e_end) does not describe a stable equilibrium of the frozen-in system attained at t = t_end = $(t_end).")
+    if any(e -> isapprox(e, e_end; atol=1e-4), fp_end)
+        idx = findmin([norm(e-e_end) for e in fp_end])[2] # the index in fp_end of the fixed point which is approximately equal to e_end
+        stab_end[idx] || @warn(
+            "The vector e_end = $(e_end) does not describe a stable equilibrium of the frozen-in system attained at t = t_end = $(t_end)."
+        )
     else
-        @warn("The vector e_end = $(e_end) does not describe a stable equilibrium of the frozen-in system attained at t = t_end = $(t_end).")
+        @warn(
+            "The vector e_end = $(e_end) does not describe a stable equilibrium of the frozen-in system attained at t = t_end = $(t_end)."
+        )
     end
 
     ##-----check no.3-----##
-    
+
     ## that
     ## the system successfully end-point tracks for r = rmin and fails to end-point track for r = rmax
     ## or
     ## the system fails to end-point track for r = rmin and successfully end point tracks for r = rmax 
 
-    rp.λ = (p,τ) -> λ_mod(p,τ)
+    rp.λ = (p, τ) -> λ_mod(p, τ)
 
-    min_rate_track = end_point_tracking(ds,rp,rmin,e_start,t_start,e_end,t_end,rad) # true if the system end-point tracks for that rate
+    min_rate_track = end_point_tracking(ds, rp, rmin, e_start, t_start, e_end, t_end, rad) # true if the system end-point tracks for that rate
 
-    max_rate_track = end_point_tracking(ds,rp,rmax,e_start,t_start,e_end,t_end,rad) # true if the system end-point tracks for that rate
+    max_rate_track = end_point_tracking(ds, rp, rmax, e_start, t_start, e_end, t_end, rad) # true if the system end-point tracks for that rate
 
-    if min_rate_track && max_rate_track 
-        error("The system successfully end-point tracks for both r = rmin = $(rmin) and r = rmax = $(rmax).")
+    if min_rate_track && max_rate_track
+        error(
+            "The system successfully end-point tracks for both r = rmin = $(rmin) and r = rmax = $(rmax).",
+        )
     elseif !min_rate_track && !max_rate_track
-        error("The system fails to end-point track for both r = rmin = $(rmin) and r = rmax = $(rmax).")
+        error(
+            "The system fails to end-point track for both r = rmin = $(rmin) and r = rmax = $(rmax).",
+        )
     elseif !min_rate_track && max_rate_track
-        @warn("The system fails to end-point track for r = rmin = $(rmin) but succesfully end-point tracks for r = rmax = $(rmax).")
+        @warn(
+            "The system fails to end-point track for r = rmin = $(rmin) but succesfully end-point tracks for r = rmax = $(rmax)."
+        )
     end
 
     ## all checks complete ##
 
     ## performing the bisection to find a critical rate within the given tolerance ##
 
-    func(r) = end_point_tracking(ds,rp,r,e_start,t_start,e_end,t_end,rad)
-    rcrit_interval = binary_bisection(func,rmin,rmax;tol,maxiter)
+    func(r) = end_point_tracking(ds, rp, r, e_start, t_start, e_end, t_end, rad)
+    rcrit_interval = binary_bisection(func, rmin, rmax; tol, maxiter)
 
     return (rcrit_interval[1]+rcrit_interval[2])/2
-
 end
 
 # beginning notes
 
 # you are trying to locate the critical rate r for which the system end-point tracks the moving branch of sinks 
 
-# the first case is where after some travel time you are within a defined neighbourhood of a sink belonging to the frozen-in system at that time 
+# the first case is where after some travel time you are within a defined neighborhood of a sink belonging to the frozen-in system at that time 
 # for this you should provide the tuple of tuples ((e₋,t₋),(e₊,t₊))
 # then you start at the position e⁻ at the time t⁻ and simulate t⁺-t⁻ time-units and compute the distance to e⁺
-# we assume that there exists some pairing (r₁,r₂) whereby the system lies within the neighbourhood of e⁺ under the rate r₁ and outside of the neighbourhood of e⁺ under the rate r₂ 
+# we assume that there exists some pairing (r₁,r₂) whereby the system lies within the neighborhood of e⁺ under the rate r₁ and outside of the neighborhood of e⁺ under the rate r₂ 
--- src/r_tipping/moving_sinks.jl
+++ src/r_tipping/moving_sinks.jl
@@ -1,17 +1,17 @@
 """
-    moving_sinks(ds::ContinuousTimeDynamicalSystem, rp::RateProtocol, box; kwargs...)
+    moving_fixedpoints(rs::RateSystem, box; kwargs...)
 
 Calculates fixed points of a nonautonomous dynamical system at snapshots in time, giving
 insight into how the equilibria change as the forcing changes over time.
 
 ## Input
-`ds` is a dynamical system of type `ContinuousTimeDynamicalSystem`, `rp` a `RateProtocol`,
+`rs` is a dynamical system of type [`RateSystem`](@ref)
 and `box` is a vector of intervals (e.g. `[interval(0,1), interval(-1,1)]`)
 that delimits the phase space volume in which to look for equilibria
-(see [`fixedpoints`](@ref)).
+(see [`Attractors.fixedpoints`](@ref)).
 
 ## Keyword arguments
-- `times = 0:0.1:1`: time points (relative to the system time `t`)
+- `Δt = 0.1`: Time step between fixed point calculations (in system time units)
 
 ## output
 Returns a triple of vectors:
@@ -21,50 +21,51 @@ Returns a triple of vectors:
 
 The term "moving sinks" refers to Wieczorek et al. (2023).
 """
-# function moving_sinks(
-#     ds::ContinuousTimeDynamicalSystem, rp::RateProtocol, box; times=0:0.1:1
-# )
-#     fp, eig, stab = [], [], []
-#     for t in times
-#         rate_sys = apply_ramping(ds, rp, t)
-#         _fp, _eig, _stab = fixedpoints(rate_sys, box)
-#         push!(fp, _fp)
-#         push!(eig, _eig)
-#         push!(stab, _stab)
-#     end
-#     return fp, eig, stab
-# end
-
-# modified version
-function moving_sinks(
-    ds::ContinuousTimeDynamicalSystem, rp::RateProtocol, box; 
-    times=rp.t_start/rp.r:(rp.t_end/rp.r-rp.t_start/rp.r)/100:rp.t_end/rp.r
-)
+function moving_fixedpoints(rs::RateSystem, box; Δt=0.1)
     fp, eig, stab = [], [], []
-    λ_mod  = rp.λ
-    t_start = rp.t_start
-    t_end = rp.t_end
+    t_start = rs.forcing.forcing_start_time
+    t_end = rs.forcing_forcing_start_time + rs.forcing.forcing_duration
+    times = t_start:Δt:t_end
+
     for t in times
-        
-        t̃ = if r*t ≤ t_start
-                t_start
-            elseif t_start < r*t < t_end
-                r*t
-            else
-                t_end
-            end; # the value of the argument of the λ function in the drift field at time t
-        
-        # the fixed point function computes fixed points for the autonomous system attained at time t = 0
-        # ensuring that rp.t_start < 0 < rp.t_end and shifting the rp.λ function so that modified drift calls λ_mod(p,t̃) = λ_mod(p,r*0+t̃), rather than λ_mod(p,t_start+t̃) or λ_mod(p,t_end+t̃) for instance
-        rp.t_start = -1.0
-        rp.t_end = 1.0
-        rp.λ = (p,τ) -> λ_mod(p,τ+t̃)
-        
-        rate_sys = apply_ramping(ds, rp, rp.t_start)
-        _fp, _eig, _stab = fixedpoints(rate_sys, box)
+        _fp, _eig, _stab = fixedpoints(frozen_system(rs, t), box)
         push!(fp, _fp)
         push!(eig, _eig)
         push!(stab, _stab)
     end
     return fp, eig, stab
 end
+
+# modified version
+#function moving_sinks(
+#    ds::ContinuousTimeDynamicalSystem, rp::RateProtocol, box; 
+#    times=rp.t_start/rp.r:(rp.t_end/rp.r-rp.t_start/rp.r)/100:rp.t_end/rp.r
+#)
+#    fp, eig, stab = [], [], []
+#    λ_mod  = rp.λ
+#    t_start = rp.t_start
+#    t_end = rp.t_end
+#    for t in times
+#        
+#        t̃ = if r*t ≤ t_start
+#                t_start
+#            elseif t_start < r*t < t_end
+#                r*t
+#            else
+#                t_end
+#            end; # the value of the argument of the λ function in the drift field at time t
+#        
+#        # the fixed point function computes fixed points for the autonomous system attained at time t = 0
+#        # ensuring that rp.t_start < 0 < rp.t_end and shifting the rp.λ function so that modified drift calls λ_mod(p,t̃) = λ_mod(p,r*0+t̃), rather than λ_mod(p,t_start+t̃) or λ_mod(p,t_end+t̃) for instance
+#        rp.t_start = -1.0
+#        rp.t_end = 1.0
+#        rp.λ = (p,τ) -> λ_mod(p,τ+t̃)
+#        
+#        rate_sys = apply_ramping(ds, rp, rp.t_start)
+#        _fp, _eig, _stab = fixedpoints(rate_sys, box)
+#        push!(fp, _fp)
+#        push!(eig, _eig)
+#        push!(stab, _stab)
+#    end
+#    return fp, eig, stab
+#end
