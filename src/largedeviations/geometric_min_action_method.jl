204a2c1738c8adc256ae4058fad2fe4121e3befa
Orjan Ameye <orjan.ameye@hotmail.com>
2026-01-24 09:31:55 +0100

    format

--- src/largedeviations/geometric_min_action_method.jl
+++ src/largedeviations/geometric_min_action_method.jl
@@ -87,7 +87,8 @@ function geometric_min_action_method(
                 curr_action = S(path)
                 if isfinite(curr_action) && isfinite(prev_action)
                     abs_change = abs(curr_action - prev_action)
-                    rel_change = curr_action == 0 ? abs_change : abs_change / abs(curr_action)
+                    rel_change =
+                        curr_action == 0 ? abs_change : abs_change / abs(curr_action)
                     if (isfinite(abstol) && abs_change < abstol) ||
                         (isfinite(reltol) && rel_change < reltol)
                         verbose &&
