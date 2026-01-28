97031bfb11e8a6c84df2fe7ff8442569f5d435ff
Orjan Ameye <orjan.ameye@hotmail.com>
2026-01-28 12:12:22 +0100

    format

--- src/largedeviations/sgMAM.jl
+++ src/largedeviations/sgMAM.jl
@@ -110,7 +110,8 @@ function simple_geometric_min_action_method(
         rel_change = S[end] == 0 ? abs_change : abs_change / abs(S[end])
         if (isfinite(abstol) && abs_change < abstol) ||
             (isfinite(reltol) && rel_change < reltol)
-            verbose && @info "Converged after $i iterations with abs=$abs_change, rel=$rel_change"
+            verbose &&
+                @info "Converged after $i iterations with abs=$abs_change, rel=$rel_change"
             break
         end
         next!(
