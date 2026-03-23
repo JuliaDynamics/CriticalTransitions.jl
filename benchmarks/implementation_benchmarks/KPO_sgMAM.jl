602f39fb8da9a1de746d308589b92778f809c8f6
Orjan Ameye <orjan.ameye@hotmail.com>
2026-03-23 10:26:27 +0100

    fix benchmarks

--- benchmarks/implementation_benchmarks/KPO_sgMAM.jl
+++ benchmarks/implementation_benchmarks/KPO_sgMAM.jl
@@ -58,8 +58,9 @@ xx = @. (xb[1] - xa[1]) * s + xa[1] + 4 * s * (1 - s) * xsaddle[1]
 yy = @. (xb[2] - xa[2]) * s + xa[2] + 4 * s * (1 - s) * xsaddle[2] + 0.01 * sin(2π * s)
 x_initial = Matrix([xx yy]')
 
+opt = GeometricGradient(; stepsize=10e2)
 MLP = simple_geometric_min_action_method(
-    sys, x_initial; maxiters=100_000, stepsize=10e2, show_progress=true
+    sys, x_initial, opt; maxiters=100_000, show_progress=true
 )
 x_min = MLP.path
 S_min = MLP.action
@@ -73,10 +74,10 @@ string_path = permutedims(Matrix(string))
 plot!(string_path[1, :], string_path[2, :]; label="string", lw=3, c=:blue)
 
 @btime $simple_geometric_min_action_method(
-    $sys, $x_initial, maxiters=100, stepsize=10e2, show_progress=false
+    $sys, $x_initial, $opt; maxiters=100, show_progress=false
 ) # 25.803 ms (29024 allocations: 105.69 MiB)
 @profview simple_geometric_min_action_method(
-    sys, x_initial, maxiters=100, stepsize=10e2, show_progress=false
+    sys, x_initial, opt; maxiters=100, show_progress=false
 )
 
 # The bottleneck is atm at the LinearSolve call to update the x in the new iteration. So the more improve, one needs to write it own LU factorization.
