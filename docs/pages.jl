a127c358b4df6c77f4e6d6177e73baab07f0c9cd
Orjan Ameye <orjan.ameye@hotmail.com>
2026-06-01 20:44:39 +0200

    feat(tpt): CTMC formulation of TPT on DiffusionGenerator (#343)

--- docs/pages.jl
+++ docs/pages.jl
@@ -12,8 +12,7 @@ pages = [
             "Quasipotential: Maier-Stein system" => "examples/quasipotential_maierstein.md",
             "Simple gMAM: Kerr Parametric Oscillator" => "examples/sgMAM_KPO.md",
             "Adaptive step-size control for sgMAM" => "examples/backtracking_KPO.md",
-            "String method: Muller-Brown potential" => "examples/potential_string.md",
-            "Transition path theory: Finite element method" => "examples/transition_path_theory_double_well.md",],
+            "String method: Muller-Brown potential" => "examples/potential_string.md",],
     ],
     "Manual" => Any[
         "Define your system" => "man/system_construction.md",
