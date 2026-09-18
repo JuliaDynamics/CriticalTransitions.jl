d6796d81a8b7dfae1133336cbfdde5223f6c872d
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-18 09:18:10 +0200

    fix JET failures

--- test/quality/JET.jl
+++ test/quality/JET.jl
@@ -2,34 +2,8 @@ using CriticalTransitions
 using Test
 using JET
 
-report_text(report) = sprint(show, MIME("text/plain"), report)
-
-matches_allowed(text, needle::AbstractString) = occursin(needle, text)
-matches_allowed(text, needle::Regex) = occursin(needle, text)
-
-unmatched(report, allowed::AbstractVector) = filter(
-    text -> !any(needle -> matches_allowed(text, needle), allowed),
-    map(report_text, JET.get_reports(report)),
-)
-
-function test_allowed_only(report, allowed::AbstractVector)
-    left = unmatched(report, allowed)
-    isempty(left) || @info "unmatched JET reports" left
-    @test isempty(left)
-    return
-end
-
 @static if isempty(VERSION.prerelease)
-    @testset "JET report_package (correctness)" begin
-        result = JET.report_package(
-            CriticalTransitions; target_modules = (CriticalTransitions,)
-        )
-        test_allowed_only(
-            result,
-            [
-                "CriticalTransitions._fill_axis!(",
-                "CriticalTransitions._solve_dirichlet",
-            ],
-        )
+    @testset "Code linting" begin
+        JET.test_package(CriticalTransitions; target_modules = (CriticalTransitions,))
     end
 end
