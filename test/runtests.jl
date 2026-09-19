7fdbdadd28e92f775cfd20fc7cca8aa3b9c02c39
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-19 18:27:43 +0200

    Include static coupled sgMAM solve regression

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,6 +25,7 @@ end
     include("largedeviations/MAM.jl")
     include("largedeviations/gMAM.jl")
     include("largedeviations/sgMAM.jl")
+    include("largedeviations/sgmam_static_solve.jl")
     include("largedeviations/hamiltonian_inplace.jl")
     include("largedeviations/multiplicative_noise.jl")
     include("largedeviations/unified_api.jl")
