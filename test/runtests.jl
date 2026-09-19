f93a57aa5835b3c1e888c43e47e3d3af516542a3
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-19 21:40:33 +0200

    Exploit block-tridiagonal structure in coupled sgMAM

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,6 +26,7 @@ end
     include("largedeviations/gMAM.jl")
     include("largedeviations/sgMAM.jl")
     include("largedeviations/sgmam_static_solve.jl")
+    include("largedeviations/sgmam_block_solve.jl")
     include("largedeviations/multiplicative_noise.jl")
     include("largedeviations/gmam_static_solve.jl")
     include("largedeviations/unified_api.jl")
