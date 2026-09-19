c28e5d13ee450475f1b05b5544556cc0ff7ea445
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-19 10:39:33 +0200

    Run path velocity regression

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,6 +31,7 @@ end
     include("largedeviations/string_method.jl")
     include("largedeviations/Maier_stein.jl")
     include("largedeviations/interpolate.jl")
+    include("largedeviations/path_velocity.jl")
     include("largedeviations/multiple_shooting.jl")
     include("largedeviations/multiple_shooting_rejection.jl")
     include("largedeviations/quasipotential.jl")
