bcc410ca1739de927cd305ec3236b0df6a372897
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-20 13:24:10 +0200

    Run target-condition scaling benchmarks

--- benchmarks/runbenchmarks.jl
+++ benchmarks/runbenchmarks.jl
@@ -6,6 +6,9 @@ const SUITE = BenchmarkGroup()
 include("transition_callbacks.jl")
 benchmark_transition_callbacks!(SUITE)
 
+include("transition_condition_scaling.jl")
+benchmark_transition_condition_scaling!(SUITE)
+
 BenchmarkTools.tune!(SUITE)
 results = BenchmarkTools.run(SUITE; verbose = true)
 display(median(results))
